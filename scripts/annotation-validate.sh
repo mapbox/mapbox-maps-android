6ac8fe7dae2725125225887db0602ff90818f60f
Peng Liu <peng.liu@mapbox.com>
2021-01-25 20:08:41 +0200

    Remove annotation generator validation scriptis.

--- scripts/annotation-validate.sh
+++ scripts/annotation-validate.sh
@@ -1,3 +1,2 @@
 #!/bin/bash -eo pipefail
-make generate-annotation-code 2>&1 | grep -i '* Updating outdated file' && { echo 'Annotation plugin validation failed, please sync the annotation plugin code and their templates.'; exit 1; }
 exit 0
