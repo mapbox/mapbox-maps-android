e3705acdb5a845d6a7245a5a9cd7c604b1d69b22
kevin <kevin.li@mapbox.com>
2021-03-01 12:07:44 +0800

    Create random points

--- app/src/main/java/com/mapbox/maps/testapp/examples/annotation/SymbolClusterActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/annotation/SymbolClusterActivity.kt
@@ -3,17 +3,17 @@ package com.mapbox.maps.testapp.examples.annotation
 import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.plugin.annotation.AnnotationConfig
 import com.mapbox.maps.plugin.annotation.AnnotationSourceOptions
 import com.mapbox.maps.plugin.annotation.ClusterOptions
-import com.mapbox.maps.plugin.annotation.generated.*
+import com.mapbox.maps.plugin.annotation.generated.SymbolManager
+import com.mapbox.maps.plugin.annotation.generated.SymbolOptions
+import com.mapbox.maps.plugin.annotation.generated.createSymbolManager
 import com.mapbox.maps.plugin.annotation.getAnnotationPlugin
 import com.mapbox.maps.testapp.R
-import kotlinx.android.synthetic.main.activity_add_marker_symbol.*
 import kotlinx.android.synthetic.main.activity_add_marker_symbol.mapView
 import kotlinx.android.synthetic.main.activity_annotation.*
 import kotlinx.coroutines.CoroutineScope
@@ -32,6 +32,7 @@ class SymbolClusterActivity : AppCompatActivity(), CoroutineScope {
   private var symbolManager: SymbolManager? = null
   private var options: List<SymbolOptions>? = null
   private var index: Int = 0
+  private val random = Random()
   private val nextStyle: String
     get() {
       return AnnotationUtils.STYLES[index++ % AnnotationUtils.STYLES.size]
@@ -49,9 +50,6 @@ class SymbolClusterActivity : AppCompatActivity(), CoroutineScope {
             .build()
         )
         loadStyleUri(nextStyle) {
-          launch {
-            loadData()
-          }
           val annotationPlugin = mapView.getAnnotationPlugin()
           val annotationConfig = AnnotationConfig(
             annotationSourceOptions = AnnotationSourceOptions(
@@ -66,6 +64,9 @@ class SymbolClusterActivity : AppCompatActivity(), CoroutineScope {
             )
           )
           symbolManager = annotationPlugin.createSymbolManager(mapView, annotationConfig)
+          launch {
+            loadData()
+          }
         }
       }
 
@@ -77,18 +78,10 @@ class SymbolClusterActivity : AppCompatActivity(), CoroutineScope {
 
   private fun loadData() {
     if (options == null) {
-      AnnotationUtils.loadStringFromAssets(
-        this@SymbolClusterActivity,
-        "points.geojson"
-      )?.let {
-        FeatureCollection.fromJson(it).features()?.let { features ->
-          features.shuffle()
-          options = features.take(AMOUNT).map { feature ->
-            SymbolOptions()
-              .withGeometry((feature.geometry() as Point))
-              .withIconImage(ICON_FIRE_STATION)
-          }
-        }
+      options = createRandomPoints().map {
+        SymbolOptions()
+          .withGeometry(it)
+          .withIconImage(ICON_FIRE_STATION)
       }
     }
     symbolManager?.deleteAll()
@@ -117,10 +110,33 @@ class SymbolClusterActivity : AppCompatActivity(), CoroutineScope {
     mapView.onDestroy()
   }
 
+  private fun createRandomPoints(): List<Point> {
+    val points: MutableList<Point> = ArrayList<Point>()
+    for (i in 0..AMOUNT) {
+      points.add(
+        Point.fromLngLat(
+          randomDouble(LONGITUDE_ORIGIN, LONGITUDE_BOUND),
+          randomDouble(LATITUDE_ORIGIN, LATITUDE_BOUND)
+        )
+      )
+    }
+    return points
+  }
+
+  private fun randomDouble(origin: Double, bound: Double): Double {
+    var r = random.nextDouble()
+    r = r * (bound - origin) + origin
+    return r
+  }
+
   companion object {
     private const val AMOUNT = 10000
     private const val ICON_FIRE_STATION = "fire-station-11"
     private const val LONGITUDE = -77.00897
     private const val LATITUDE = 38.87031
+    private const val LONGITUDE_ORIGIN = -77.1799850463867
+    private const val LONGITUDE_BOUND = -76.90841674804688
+    private const val LATITUDE_ORIGIN = 38.799584013897054
+    private const val LATITUDE_BOUND = 38.992237864985896
   }
 }
