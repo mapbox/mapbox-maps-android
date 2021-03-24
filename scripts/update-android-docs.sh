755c94e39c134e9b3cfc727118c11c11b8fe819e
kevin <kevin.li@mapbox.com>
2021-03-24 16:34:25 +0800

    Add auto doc config for ci

--- scripts/update-android-docs.sh
+++ scripts/update-android-docs.sh
@@ -22,106 +22,107 @@ readonly ANDROID_DOCS_REPO="git@github.com:mapbox/android-docs.git"
 
 MAPS_SDK_VERSION=
 REVIEWERS=
-ANDROID_DOCS_REPO_URL=
 
-PROG=`basename $0`
+PROG=$(basename $0)
 SHORT_OPTS=s:a:r:h
 LONG_OPTS=sdk-version:,reviewers:,help
 
 function usage() {
-    message="Usage: $PROG -s SDK_VERSION -a ANDROID_DOCS_REPO\
+  message="Usage: $PROG -s SDK_VERSION \
            \nParameters:\
            \n  -s | --sdk-version       [version]                      Maps SDK version.\
            \n  -r | --reviewers         [gh_usr1,gh_usr2,org/team,...] Comma-separated list of reviewer (github logins).\
            \n  -h | --help                                             Shows this help message."
-    echo -e "$message"
+  echo -e "$message"
 }
 
 if ! getopt --name "$PROG" --long $LONG_OPTS --options $SHORT_OPTS -- "$@" >/dev/null; then
-    echo "$PROG: usage error (use -h or --help for help)" >&2
-    exit 1
+  echo "$PROG: usage error (use -h or --help for help)" >&2
+  exit 1
 fi
-ARGS=`getopt --name "$PROG" --long $LONG_OPTS --options $SHORT_OPTS -- "$@"`
+ARGS=$(getopt --name "$PROG" --long $LONG_OPTS --options $SHORT_OPTS -- "$@")
 
 while [ $# -gt 0 ]; do
-    case "$1" in
-        -s | --sdk-version) MAPS_SDK_VERSION="$2"; shift;;
-        -r | --reviewers) REVIEWERS="$2"; shift;;
-        -h | --help) usage; exit 0;;
-        --) shift; break;;
-    esac
-    shift
+  case "$1" in
+      -s | --sdk-version) MAPS_SDK_VERSION="$2"; shift;;
+      -r | --reviewers) REVIEWERS="$2"; shift;;
+      -h | --help) usage; exit 0;;
+      --) shift; break;;
+  esac
+  shift
 done
 
 if [ -z "$MAPS_SDK_VERSION" ]; then
-    echo -e "ERROR: Map SDK version not defined.\n"
-    usage
-    exit 1
+  echo -e "ERROR: Map SDK version not defined.\n"
+  usage
+  exit 1
 fi
 
 function generate_docs() {
-    make dokka-html || dokka_result=1
+  make dokka-html || dokka_result=1
 
-    if [ ${dokka_result:-0} -eq 1 ]
-    then
-        echo "dokka failed, ignoring."
-    fi
+  if [ ${dokka_result:-0} -eq 1 ]; then
+    echo "dokka failed, ignoring."
+  fi
 
-    if [ ! -d $DOKKA_OUTPUT_DIR ]; then
-        echo "Directory with docs doesn't exist."
-        exit 1
-    fi
+  if [ ! -d $DOKKA_OUTPUT_DIR ]; then
+    echo "Directory with docs doesn't exist."
+    exit 1
+  fi
 }
 
 function prepare_branch_with_documentation() {
-    INTERIM_BRANCH_WITH_DOCUMENTATION="${BRANCH_WITH_DOCUMENTATION}_${1}"
-    git checkout $BRANCH_WITH_DOCUMENTATION
-    mkdir -p $1
-    cp -r $DOKKA_OUTPUT_DIR/* $1
-    git add $1
-    git commit -m "Add $1 API documentation."
-    git checkout -b $INTERIM_BRANCH_WITH_DOCUMENTATION
-    git push --set-upstream origin $INTERIM_BRANCH_WITH_DOCUMENTATION
+  INTERIM_BRANCH_WITH_DOCUMENTATION="${BRANCH_WITH_DOCUMENTATION}_${1}"
+  git checkout $BRANCH_WITH_DOCUMENTATION
+  mkdir -p $1
+  cp -r $DOKKA_OUTPUT_DIR/* $1
+  git add $1
+  git commit -m "Add $1 API documentation."
+  git checkout -b $INTERIM_BRANCH_WITH_DOCUMENTATION
+  git push --set-upstream origin $INTERIM_BRANCH_WITH_DOCUMENTATION
 }
 
 function clone_android_docs_repo() {
-    git clone $ANDROID_DOCS_REPO_URL $ANDROID_DOCS_DIRECTORY
+  git clone $ANDROID_DOCS_REPO $ANDROID_DOCS_DIRECTORY
 }
 
 function update_constants_and_map_version_numbers() {
-    cd $ANDROID_DOCS_DIRECTORY
-    CONSTANTS_FILE_TMP="${CONSTANTS_FILE}.tmp"
-    jq --arg version $1 '.MAP_SDK_VERSION=$version' $CONSTANTS_FILE > $CONSTANTS_FILE_TMP
-    mv $CONSTANTS_FILE_TMP $CONSTANTS_FILE
+  cd $ANDROID_DOCS_DIRECTORY
+  CONSTANTS_FILE_TMP="${CONSTANTS_FILE}.tmp"
+  jq --arg version $1 '.MAP_SDK_v10_VERSION=$version' $CONSTANTS_FILE >$CONSTANTS_FILE_TMP
+  mv $CONSTANTS_FILE_TMP $CONSTANTS_FILE
 
-    MAP_VERSION_NUMBERS_FILE_TMP="${MAP_VERSION_NUMBERS_FILE}.tmp"
-    jq --arg version $1 '. += [$version]' $MAP_VERSION_NUMBERS_FILE > $MAP_VERSION_NUMBERS_FILE_TMP
-    mv $MAP_VERSION_NUMBERS_FILE_TMP $MAP_VERSION_NUMBERS_FILE
-    cd -
+  MAP_VERSION_NUMBERS_FILE_TMP="${MAP_VERSION_NUMBERS_FILE}.tmp"
+  if [[ $1 == *beta* ]] || [[ $1 == *alpha* ]]; then
+    jq --arg version $1 '.[-1] |= $version' $MAP_VERSION_NUMBERS_FILE >$MAP_VERSION_NUMBERS_FILE_TMP
+  else
+    jq --arg version $1 '. += [$version]' $MAP_VERSION_NUMBERS_FILE >$MAP_VERSION_NUMBERS_FILE_TMP
+  fi
+  mv $MAP_VERSION_NUMBERS_FILE_TMP $MAP_VERSION_NUMBERS_FILE
+  cd -
 }
 
 function prepare_android_docs_branch() {
-    cd $ANDROID_DOCS_DIRECTORY
-    BRANCH_NAME="maps_android_sdk_v$1"
-    git checkout -b $BRANCH_NAME
-    git add -A
-    git commit -m "Carbon Maps SDK bump to $1"
-    git push --set-upstream origin $BRANCH_NAME
-    cd -
+  cd $ANDROID_DOCS_DIRECTORY
+  BRANCH_NAME="maps_android_sdk_v$1"
+  git checkout -b $BRANCH_NAME
+  git add -A
+  git commit -m "Carbon Maps SDK bump to $1"
+  git push --set-upstream origin $BRANCH_NAME
+  cd -
 }
 
 function create_pull_request() {
-    CMD="gh pr create --title \"${1}\" --body \"\""
+  CMD="gh pr create --title \"${1}\" --body \"\""
 
-    if [ ! -z "$REVIEWERS" ]; then
-        CMD+=" --reviewer ${REVIEWERS}"
-    fi
+  if [ ! -z "$REVIEWERS" ]; then
+    CMD+=" --reviewer ${REVIEWERS}"
+  fi
 
-    if [ ! -z "${2:-}" ]; then
-        CMD+=" --base ${BRANCH_WITH_DOCUMENTATION}"
-    fi
-    echo $CMD
-    #eval $CMD
+  if [ ! -z "${2:-}" ]; then
+    CMD+=" --base ${BRANCH_WITH_DOCUMENTATION}"
+  fi
+  eval $CMD
 }
 
 # Generate docs, create branch and make PR with API documentation in the SDK repo.
@@ -129,7 +130,7 @@ generate_docs
 prepare_branch_with_documentation $MAPS_SDK_VERSION
 create_pull_request "Add ${MAPS_SDK_VERSION} API documentation." $BRANCH_WITH_DOCUMENTATION
 
-# Update config files in Android Docs Repo.
+## Update config files in Android Docs Repo.
 clone_android_docs_repo
 update_constants_and_map_version_numbers $MAPS_SDK_VERSION
 prepare_android_docs_branch $MAPS_SDK_VERSION
