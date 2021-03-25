2b2c752f3c3cfd17c2828f752bf8a19dc1807ea3
kevin <kevin.li@mapbox.com>
2021-03-25 17:46:11 +0800

    Add gh login with token

--- scripts/update-android-docs.sh
+++ scripts/update-android-docs.sh
@@ -113,7 +113,7 @@ function prepare_android_docs_branch() {
 }
 
 function create_pull_request() {
-  gh auth login
+  gh auth login --with-token < .gh_token.txt
   CMD="gh pr create --title \"${1}\" --body \"\""
 
   if [ ! -z "$REVIEWERS" ]; then
