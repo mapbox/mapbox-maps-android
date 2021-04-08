84a343ca6a3873f8f8c06f585ef5bcc1b7e07dc1
kevin <kevin.li@mapbox.com>
2021-04-08 19:02:44 +0800

    Fix review comments

--- sdk/src/main/java/com/mapbox/maps/CredentialsManager.kt
+++ sdk/src/main/java/com/mapbox/maps/CredentialsManager.kt
@@ -3,32 +3,32 @@ package com.mapbox.maps
 import android.content.Context
 
 /**
- * Convenience class that holds MapboxMaps related token.
- * It's possible to create `CredentialsManager` instances as you need them,
- * however it's convenient to use the default `CredentialsManager.shared`.
+ * Convenience class that holds [MapboxMap] related token.
+ * `CredentialsManager` could be created per `MapView` instance if given view should use some specific token.
+ * Most common use-case when all `MapView`s should use same token could be handled by using `CredentialsManager.shared` static object.
  *
  * @property accessToken the token will be applied.
  */
 data class CredentialsManager(val accessToken: String? = null) {
   /**
    * Get the access token.
-   * If the supplied token is nil (which is the case for the `shared`),
-   * then we will search for an access token in the application resource.
+   * If the supplied token is null (which is the use-case for the `shared`),
+   * then we will search for an access token in the application resources.
    */
   fun getAccessToken(context: Context): String {
     accessToken?.let {
-      // User have set the token, use this one.
+      // User has set the token, use it.
       return it
     }
 
     // Check in the resources
     val tokenResId = context.resources.getIdentifier(
       MAPBOX_ACCESS_TOKEN_RESOURCE_NAME,
-      "string",
+      STRING_DEF_TYPE,
       context.packageName
     )
 
-    return if (tokenResId != 0) context.getString(tokenResId) else ""
+    return if (tokenResId != 0) context.getString(tokenResId) else EMPTY_TOKEN
   }
 
   /**
@@ -36,8 +36,10 @@ data class CredentialsManager(val accessToken: String? = null) {
    */
   companion object {
     /**
-     * The default shared instance with empty assess token.
+     * The default shared instance with empty assess token, will search for an access token in the application resources.
      */
     var shared: CredentialsManager = CredentialsManager()
+    internal const val EMPTY_TOKEN = ""
+    private const val STRING_DEF_TYPE = "string"
   }
 }
