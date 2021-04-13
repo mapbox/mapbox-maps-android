a4aab68f1ea5ec0374758ad9a68627c5b7d3559d
Kevin Li <kevin.li@mapbox.com>
2021-04-13 16:00:13 +0800

    Make overlay plugin experimental (#233)

---
+++ sdk-base/src/main/java/com/mapbox/maps/MapboxExperimental.kt
@@ -0,0 +1,12 @@
+package com.mapbox.maps
+
+/**
+ * Annotation class to mark API as experimental.
+ */
+@RequiresOptIn(
+  level = RequiresOptIn.Level.WARNING,
+  message = "This API is experimental. It may be changed in the future without notice."
+)
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+annotation class MapboxExperimental
