3facc82429203845411035e5afc8c296a4cf946e
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-04-29 16:06:06 +0300

    Add activity to test large geojson performance

---
+++ app/src/main/java/com/mapbox/maps/testapp/examples/LargeGeojsonPerformanceActivity.kt
@@ -0,0 +1,120 @@
+package com.mapbox.maps.testapp.examples
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.api.directions.v5.models.DirectionsResponse
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
+import com.mapbox.geojson.LineString
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.Style
+import com.mapbox.maps.extension.style.layers.addLayer
+import com.mapbox.maps.extension.style.layers.generated.lineLayer
+import com.mapbox.maps.extension.style.sources.addSource
+import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
+import com.mapbox.maps.plugin.animation.MapAnimationOptions
+import com.mapbox.maps.plugin.animation.flyTo
+import com.mapbox.maps.testapp.R
+import com.mapbox.maps.testapp.examples.annotation.AnnotationUtils
+import kotlinx.android.synthetic.main.activity_large_geojson.*
+
+/**
+ * Example of passing large geojson to verify it does not block UI thread.
+ */
+class LargeGeojsonPerformanceActivity : AppCompatActivity() {
+
+  private lateinit var routePoints: FeatureCollection
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_large_geojson)
+
+    routePoints = FeatureCollection.fromFeature(
+      Feature.fromGeometry(
+        LineString.fromPolyline(
+          DirectionsResponse.fromJson(
+            AnnotationUtils.loadStringFromAssets(
+              this@LargeGeojsonPerformanceActivity, "looong.json"
+            )
+          ).routes()[0].geometry()!!,
+          6
+        )
+      )
+    )
+
+    mapView.getMapboxMap()
+      .apply {
+        setCamera(
+          CameraOptions.Builder()
+            .center(Point.fromLngLat(LONGITUDE, LATITUDE))
+            .zoom(START_ZOOM)
+            .build()
+        )
+        loadStyleUri(Style.MAPBOX_STREETS) { style ->
+          buttonLoad.setOnClickListener {
+            flyTo(
+              CameraOptions.Builder()
+                .zoom(END_ZOOM)
+                .build(),
+              MapAnimationOptions.mapAnimationOptions {
+                duration(10_000L)
+              })
+            it.postDelayed({
+              loadGeoJson(style)
+            }, 1000)
+          }
+        }
+      }
+  }
+
+  private fun loadGeoJson(style: Style) {
+    val start = System.currentTimeMillis()
+    style.addSource(geoJsonSource(SOURCE) {
+      featureCollection(routePoints)
+    })
+    // add some extra work to parse geojson
+    for (i in 0..5) {
+      style.addSource(geoJsonSource(i.toString()) {
+        featureCollection(routePoints)
+      })
+    }
+    style.addLayer(lineLayer(LAYER, SOURCE) {
+      lineColor("blue")
+    })
+    val end = System.currentTimeMillis()
+    Toast.makeText(this, "Parsing time: ${end - start}", Toast.LENGTH_SHORT).show()
+    buttonLoad.visibility = View.GONE
+  }
+
+  override fun onStart() {
+    super.onStart()
+    mapView.onStart()
+  }
+
+  override fun onStop() {
+    super.onStop()
+    mapView.onStop()
+  }
+
+  override fun onLowMemory() {
+    super.onLowMemory()
+    mapView.onLowMemory()
+  }
+
+  override fun onDestroy() {
+    super.onDestroy()
+    mapView.onDestroy()
+  }
+
+  companion object {
+    private const val LATITUDE = 51.1079
+    private const val LONGITUDE = 17.0385
+    private const val START_ZOOM = 6.0
+    private const val END_ZOOM = 2.0
+    private const val SOURCE = "source"
+    private const val LAYER = "layer"
+  }
+}
