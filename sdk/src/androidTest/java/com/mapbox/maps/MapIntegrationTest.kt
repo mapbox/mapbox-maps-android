fbbc7e5345d127cdf784a9d7886db4ca32c6cbb8
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-07-08 15:43:28 +0300

    Fix overwriting sync geojson data with getSourceAs by async

--- sdk/src/androidTest/java/com/mapbox/maps/MapIntegrationTest.kt
+++ sdk/src/androidTest/java/com/mapbox/maps/MapIntegrationTest.kt
@@ -10,7 +10,9 @@ import com.mapbox.geojson.Point
 import com.mapbox.maps.extension.style.layers.addLayer
 import com.mapbox.maps.extension.style.layers.generated.circleLayer
 import com.mapbox.maps.extension.style.sources.addSource
+import com.mapbox.maps.extension.style.sources.generated.GeoJsonSource
 import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
+import com.mapbox.maps.extension.style.sources.getSourceAs
 import com.mapbox.maps.extension.style.style
 import org.junit.After
 import org.junit.Before
@@ -399,6 +401,76 @@ class MapIntegrationTest {
               1_000L
             )
           }
+        }
+        mapView.onStart()
+      }
+    }
+    if (!countDownLatch.await(5, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+  }
+
+  @Test
+  fun testApplySyncDataToGeoJsonUsingGetSourceAs() {
+    countDownLatch = CountDownLatch(1)
+    rule.scenario.onActivity {
+      it.runOnUiThread {
+        mapView = MapView(it)
+        mapboxMap = mapView.getMapboxMap()
+        it.frameLayout.addView(mapView)
+        mapboxMap.setCamera(
+          CameraOptions.Builder()
+            .center(Point.fromLngLat(0.0, 0.0))
+            .zoom(9.0)
+            .build()
+        )
+        mapboxMap.loadStyleUri(Style.MAPBOX_STREETS) { style ->
+          val source = geoJsonSource("source") {
+            featureCollection(FeatureCollection.fromFeatures(emptyList()))
+          }
+          style.addSource(source)
+          val layer = circleLayer("layer", "source") {
+            circleColor("red")
+            circleRadius(10.0)
+          }
+          style.addLayer(layer)
+          val feature = Feature.fromJson(
+            """
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [0.0, 0.0]
+                    },
+                    "properties": {
+                        "fillOpacity": 0.5,
+                        "fillColor": "rgba(255, 0, 255, 1.0)",
+                        "fillPattern": "",
+                        "type": "circle",
+                        "radius": 1.0
+                    }
+                }
+            """.trimIndent()
+          )
+          style.getSourceAs<GeoJsonSource>("source")!!
+            .featureCollection(
+              FeatureCollection.fromFeatures(
+                listOf(feature)
+              )
+            )
+          mapView.postDelayed(
+            {
+              mapboxMap.queryRenderedFeatures(
+                ScreenCoordinate(mapView.width / 2.0, mapView.height / 2.0),
+                RenderedQueryOptions(listOf("layer"), null)
+              ) { result ->
+                if (result.value?.size == 1) {
+                  countDownLatch.countDown()
+                }
+              }
+            },
+            1_000L
+          )
         }
         mapView.onStart()
       }
