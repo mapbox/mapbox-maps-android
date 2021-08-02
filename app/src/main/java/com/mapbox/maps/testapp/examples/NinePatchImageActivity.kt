5463af00f20b55074c9a6378994bdabd9321068b
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-08-02 15:16:08 +0300

    Finalize example

--- app/src/main/java/com/mapbox/maps/testapp/examples/NinePatchImageActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/NinePatchImageActivity.kt
@@ -1,25 +1,30 @@
-package com.mapbox.maps.testapp.examples.markersandcallouts
+package com.mapbox.maps.testapp.examples
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Feature
-import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.Style
-import com.mapbox.maps.extension.style.expressions.dsl.generated.match
 import com.mapbox.maps.extension.style.image.image
+import com.mapbox.maps.extension.style.layers.generated.SymbolLayer
 import com.mapbox.maps.extension.style.layers.generated.symbolLayer
-import com.mapbox.maps.extension.style.layers.properties.generated.IconAnchor
+import com.mapbox.maps.extension.style.layers.getLayer
+import com.mapbox.maps.extension.style.layers.properties.generated.IconTextFit
 import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
 import com.mapbox.maps.extension.style.style
 import com.mapbox.maps.testapp.R
 import kotlinx.android.synthetic.main.activity_add_marker_symbol.*
 
 /**
- * Example showcasing of adding 9-patch images to style.
+ * Example showcasing of adding 9-patch image to style
+ * and demonstrating how it works when stretching image.
  */
 class NinePatchImageActivity : AppCompatActivity() {
+
+  private var appendTextCounter = 1
+  private lateinit var style: Style
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -27,102 +32,55 @@ class NinePatchImageActivity : AppCompatActivity() {
 
     mapView.getMapboxMap().loadStyle(
       styleExtension = style(Style.MAPBOX_STREETS) {
-        +image(RED_ICON_ID) {
-          bitmap(BitmapFactory.decodeResource(resources, R.drawable.red_marker))
+        +image(NINE_PATCH_ID) {
+          bitmapNinePatch(BitmapFactory.decodeResource(resources, R.drawable.blue_round_nine))
         }
-        // prepare blue marker from resources
-        +image(BLUE_ICON_ID) {
-          bitmap(BitmapFactory.decodeResource(resources, R.drawable.blue_marker_view))
-        }
-        // prepare source that will hold icons and add extra string property to each of it
-        // to identify what marker icon should be used
         +geoJsonSource(SOURCE_ID) {
-          featureCollection(
-            FeatureCollection.fromFeatures(
-              arrayOf(
-                Feature.fromGeometry(
-                  Point.fromLngLat(
-                    12.554729,
-                    55.70651
-                  )
-                ).apply {
-                  addStringProperty(ICON_KEY, ICON_RED_PROPERTY)
-                },
-                Feature.fromGeometry(
-                  Point.fromLngLat(
-                    12.65147,
-                    55.608166
-                  )
-                ).apply {
-                  addStringProperty(ICON_KEY, ICON_BLUE_PROPERTY)
-                }
-              )
-            )
-          )
+          feature(Feature.fromGeometry(CENTER))
         }
         +symbolLayer(LAYER_ID, SOURCE_ID) {
-          iconImage(
-            match {
-              get {
-                literal(ICON_KEY)
-              }
-              stop {
-                literal(ICON_RED_PROPERTY)
-                literal(RED_ICON_ID)
-              }
-              stop {
-                literal(ICON_BLUE_PROPERTY)
-                literal(BLUE_ICON_ID)
-              }
-              literal(RED_ICON_ID)
-            }
-          )
-          iconRotate(
-            match {
-              get {
-                literal(ICON_KEY)
-              }
-              stop {
-                literal(ICON_BLUE_PROPERTY)
-                literal(45.0)
-              }
-              literal(0.0)
-            }
-          )
-          iconAllowOverlap(true)
-          iconAnchor(IconAnchor.BOTTOM)
+          iconImage(NINE_PATCH_ID)
+          // make sure we stretch image both in X and Y
+          iconTextFit(IconTextFit.BOTH)
+          iconTextFitPadding(listOf(10.0, 10.0, 10.0, 10.0))
+          textField(TEXT_BASE)
         }
       }
+    ) {
+      style = it
+      updateIconText()
+    }
+    mapView.getMapboxMap().setCamera(
+      CameraOptions.Builder()
+        .center(CENTER)
+        .zoom(ZOOM)
+        .build()
     )
   }
 
-  override fun onStart() {
-    super.onStart()
-    mapView.onStart()
-  }
-
-  override fun onStop() {
-    super.onStop()
-    mapView.onStop()
-  }
-
-  override fun onLowMemory() {
-    super.onLowMemory()
-    mapView.onLowMemory()
-  }
-
-  override fun onDestroy() {
-    super.onDestroy()
-    mapView.onDestroy()
+  // start appending text in a loop, stretching icon in both X and Y
+  private fun updateIconText() {
+    mapView.postDelayed(
+      {
+        val layer = (style.getLayer(LAYER_ID) as SymbolLayer)
+        layer.textField("${layer.textField?.getTextAsString()} $TEXT_BASE")
+        appendTextCounter++
+        if (appendTextCounter.rem(3) == 0) {
+          layer.textField("${layer.textField?.getTextAsString()}\n")
+        }
+        updateIconText()
+      },
+      TEXT_UPDATE_TIME_MS
+    )
   }
 
   companion object {
-    private const val RED_ICON_ID = "red"
-    private const val BLUE_ICON_ID = "blue"
+    private const val NINE_PATCH_ID = "red"
     private const val SOURCE_ID = "source_id"
     private const val LAYER_ID = "layer_id"
-    private const val ICON_KEY = "icon_key"
-    private const val ICON_RED_PROPERTY = "icon_red_property"
-    private const val ICON_BLUE_PROPERTY = "icon_blue_property"
+    private val CENTER = Point.fromLngLat(12.554729, 55.70651)
+    private const val ZOOM = 9.0
+    private const val TEXT_BASE = "Hi!"
+    private const val TEXT_UPDATE_TIME_MS = 1500L
   }
 }
