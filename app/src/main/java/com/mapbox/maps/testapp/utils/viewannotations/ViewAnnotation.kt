3188e1ce5b79624632260c9be14f4eb44b2e086d
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-08-17 11:48:07 +0300

    Differentiate callout and native views

--- app/src/main/java/com/mapbox/maps/testapp/utils/viewannotations/ViewAnnotation.kt
+++ app/src/main/java/com/mapbox/maps/testapp/utils/viewannotations/ViewAnnotation.kt
@@ -2,13 +2,16 @@ package com.mapbox.maps.testapp.utils.viewannotations
 
 import android.view.View
 import android.widget.FrameLayout
+import com.mapbox.geojson.Geometry
 import com.mapbox.geojson.Point
 
 data class ViewAnnotation(
   val view: View,
   val viewLayoutParams: FrameLayout.LayoutParams,
+  val descriptor: ViewAnnotationDescriptor,
   val width: Int,
   val height: Int,
   // bunch of potential properties about placing annotation etc
-  val point: Point
+  // TODO for prototype supporting only Point for now
+  val geometry: Geometry
 )
--- app/src/main/java/com/mapbox/maps/testapp/utils/viewannotations/ViewAnnotationPlugin.kt
+++ app/src/main/java/com/mapbox/maps/testapp/utils/viewannotations/ViewAnnotationPlugin.kt
@@ -4,16 +4,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
 import androidx.annotation.LayoutRes
+import androidx.asynclayoutinflater.view.AsyncLayoutInflater
 import androidx.core.view.updateMargins
+import com.mapbox.geojson.Geometry
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.plugin.delegates.listeners.OnCameraChangeListener
-import com.mapbox.maps.testapp.R
 
 class ViewAnnotationPlugin(
   val mapView: MapView
 ): OnCameraChangeListener {
 
+  // using async inflater if needed to free up main thread a bit
+  private val asyncInflater by lazy { AsyncLayoutInflater(mapView.context) }
   // we need id to pass it to gl-native and not pass view itself as it's useless
   private val annotations = LinkedHashMap<Int, ViewAnnotation>()
 
@@ -22,50 +25,77 @@ class ViewAnnotationPlugin(
     mapView.getMapboxMap().addOnCameraChangeListener(this)
   }
 
-  // not sure if it's useful
-  fun addViewAnnotation(view: View, width: Int, height: Int, point: Point) {
-    val layoutParams = FrameLayout.LayoutParams(
-      view.layoutParams.width,
-      view.layoutParams.height
-    )
-    if (layoutParams.width == 0 || layoutParams.height == 0) {
-      throw RuntimeException("Layout params width and height must be != 0!")
-    }
-    val annotation = ViewAnnotation(
-      view = view,
-      viewLayoutParams = layoutParams,
-      width = width,
-      height = height,
-      point = point
-    )
-    annotations[view.hashCode()] = annotation
-    displayAnnotation(annotation)
-  }
-
-  // preferable way to add annotations - simply provide us layout
-  // so that we inflate it and return actual [View]
-  fun addViewAnnotation(@LayoutRes id: Int, point: Point): View {
-    // inflate only once and save view to some collection
-    val nativeView = LayoutInflater.from(mapView.context)
-      .inflate(R.layout.item_callout_view, mapView, false)
+  // synchronous method
+  fun addViewAnnotation(
+    @LayoutRes id: Int,
+    type: ViewAnnotationType,
+    geometry: Geometry
+  ): View {
+    val nativeView = LayoutInflater.from(mapView.context).inflate(id, mapView, false)
     val nativeViewLayout = nativeView.layoutParams as FrameLayout.LayoutParams
     val annotation = ViewAnnotation(
       view = nativeView,
       viewLayoutParams = nativeViewLayout,
+      descriptor = when(type) {
+        ViewAnnotationType.NATIVE -> ViewAnnotationDescriptor.Native(mapView.getMapboxMap().cameraState.bearing.toFloat())
+        ViewAnnotationType.CALLOUT -> ViewAnnotationDescriptor.Callout
+      },
       width = nativeView.layoutParams.width,
       height = nativeView.layoutParams.height,
-      point = point
+      geometry = geometry
     )
     annotations[nativeView.hashCode()] = annotation
     displayAnnotation(annotation)
     return nativeView
   }
 
+  // asynchronous method
+  fun addViewAnnotation(
+    @LayoutRes id: Int,
+    type: ViewAnnotationType,
+    geometry: Geometry,
+    result: (View) -> Unit
+  ) {
+    asyncInflater.inflate(id, mapView) { nativeView, _, _ ->
+      val nativeViewLayout = nativeView.layoutParams as FrameLayout.LayoutParams
+      val annotation = ViewAnnotation(
+        view = nativeView,
+        viewLayoutParams = nativeViewLayout,
+        descriptor = when(type) {
+          ViewAnnotationType.NATIVE -> ViewAnnotationDescriptor.Native(mapView.getMapboxMap().cameraState.bearing.toFloat())
+          ViewAnnotationType.CALLOUT -> ViewAnnotationDescriptor.Callout
+        },
+        width = nativeView.layoutParams.width,
+        height = nativeView.layoutParams.height,
+        geometry = geometry
+      )
+      annotations[nativeView.hashCode()] = annotation
+      displayAnnotation(annotation)
+      result.invoke(nativeView)
+    }
+  }
+
   // that's a temp solution to initially display view annotation on map (if map camera is not changing)
   // will be removed with new gl-native API
   private fun displayAnnotation(annotation: ViewAnnotation) {
-    val leftTop = mapView.getMapboxMap().pixelForCoordinate(annotation.point)
+    val point = annotation.geometry as Point
+    val leftTop = mapView.getMapboxMap().pixelForCoordinate(point)
+    // correctly position the view
     annotation.viewLayoutParams.updateMargins(leftTop.x.toInt(), leftTop.y.toInt(), 0, 0)
+    when (annotation.descriptor) {
+      ViewAnnotationDescriptor.Callout -> {
+        // TBD but nothing comes to mind right now
+      }
+      is ViewAnnotationDescriptor.Native -> {
+        // TODO pivot needs to be f(anchor), not anchor is left-top hardcoded so hardcoding zeros
+        annotation.view.pivotX = 0f
+        annotation.view.pivotY = 0f
+        // do not depend on current map bearing on initial view adding
+        annotation.view.rotation = 0f
+
+        annotation.view.rotationX = mapView.getMapboxMap().cameraState.pitch.toFloat()
+      }
+    }
     mapView.addView(annotation.view, annotation.viewLayoutParams)
   }
 
@@ -75,21 +105,36 @@ class ViewAnnotationPlugin(
 
   fun destroy() {
     mapView.getMapboxMap().removeOnCameraChangeListener(this)
+    annotations.clear()
   }
 
   // this should be some new API from gl-native returning array of [id, ScreenCoordinate]
   // however it terms of prototyping we use camera change callback and calculate ScreenCoordinate by ourselves
   override fun onCameraChanged() {
     annotations.forEach {
-      mapView.removeView(it.value.view)
-      // we must have new callback from gl-native returning ORDERED array of [viewId, ScreenCoordinate]
-      // in this case we calculate it ourselves (bind to hardcoded point)
-      val leftTopInner = mapView.getMapboxMap().pixelForCoordinate(it.value.point)
-      val visibleOnX = leftTopInner.x >= -it.value.viewLayoutParams.width && leftTopInner.x <= mapView.getMapboxMap().getSize().width
-      val visibleOnY = leftTopInner.y >= -it.value.viewLayoutParams.height && leftTopInner.y <= mapView.getMapboxMap().getSize().height
-      if (visibleOnX && visibleOnY) {
-        it.value.viewLayoutParams.updateMargins(leftTopInner.x.toInt(), leftTopInner.y.toInt(), 0, 0)
-        mapView.addView(it.value.view, it.value.viewLayoutParams)
+      with(it.value) {
+        mapView.removeView(view)
+        val point = geometry as Point
+        // we must have new callback from gl-native returning ORDERED array of [viewId, ScreenCoordinate]
+        // in this case we calculate it ourselves (bind to hardcoded point)
+        val leftTopInner = mapView.getMapboxMap().pixelForCoordinate(point)
+        // TODO ideally need to depend on transformed dimensions (take pitch / bearing into account)
+        val visibleOnX = leftTopInner.x >= -viewLayoutParams.width && leftTopInner.x <= mapView.getMapboxMap().getSize().width
+        val visibleOnY = leftTopInner.y >= -viewLayoutParams.height && leftTopInner.y <= mapView.getMapboxMap().getSize().height
+        if (visibleOnX && visibleOnY) {
+          viewLayoutParams.updateMargins(leftTopInner.x.toInt(), leftTopInner.y.toInt(), 0, 0)
+          when (descriptor) {
+            ViewAnnotationDescriptor.Callout -> {
+              // TBD but nothing comes to mind right now
+            }
+            is ViewAnnotationDescriptor.Native -> {
+              // do not depend on current map bearing on initial view adding
+              view.rotation = -(mapView.getMapboxMap().cameraState.bearing.toFloat() - descriptor.initialBearing)
+              view.rotationX = mapView.getMapboxMap().cameraState.pitch.toFloat()
+            }
+          }
+          mapView.addView(it.value.view, it.value.viewLayoutParams)
+        }
       }
     }
   }
