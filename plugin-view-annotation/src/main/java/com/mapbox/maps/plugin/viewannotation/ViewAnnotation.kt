825f025a2d9a14754bfb0898acdb55dd79d37c73
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-08-19 15:25:17 +0300

    API adjustments

--- plugin-view-annotation/src/main/java/com/mapbox/maps/plugin/viewannotation/ViewAnnotation.kt
+++ plugin-view-annotation/src/main/java/com/mapbox/maps/plugin/viewannotation/ViewAnnotation.kt
@@ -8,8 +8,6 @@ internal data class ViewAnnotation(
   val view: View,
   var viewLayoutParams: FrameLayout.LayoutParams,
   val descriptor: ViewAnnotationDescriptor,
-  val initialWidth: Int, // TODO will be removed as will be stored in gl-native, needed for correct resize based on zoom
-  val initialHeight: Int, // TODO will be removed as will be stored in gl-native, needed for correct resize based on zoom
   val geometry: Geometry, // TODO for prototype supporting only Point for now
   val options: ViewAnnotationOptions
 )
