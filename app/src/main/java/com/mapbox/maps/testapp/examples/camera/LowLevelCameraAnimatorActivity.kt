b434e0257d2865de3410e800197c2d74c4ef7826
Ankur Khandelwal <ankur.khandelwal@mapbox.com>
2021-08-23 11:40:43 +0300

    example align visuals for category camera

--- app/src/main/java/com/mapbox/maps/testapp/examples/camera/LowLevelCameraAnimatorActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/camera/LowLevelCameraAnimatorActivity.kt
@@ -1,23 +1,10 @@
-package com.mapbox.maps.testapp.examples
+package com.mapbox.maps.testapp.examples.camera
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.AnticipateOvershootInterpolator
-import android.view.animation.BounceInterpolator
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
-import com.mapbox.maps.extension.style.expressions.dsl.generated.literal
-import com.mapbox.maps.extension.style.expressions.dsl.generated.zoom
-import com.mapbox.maps.extension.style.expressions.generated.Expression
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.eq
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.exponential
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.get
-import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.interpolate
-import com.mapbox.maps.extension.style.layers.addLayer
-import com.mapbox.maps.extension.style.layers.generated.FillExtrusionLayer
-import com.mapbox.maps.extension.style.light.generated.getLight
 import com.mapbox.maps.plugin.animation.CameraAnimatorOptions.Companion.cameraAnimatorOptions
 import com.mapbox.maps.plugin.animation.camera
 import com.mapbox.maps.testapp.databinding.ActivityCameraAnimateBinding
@@ -35,23 +22,22 @@ class LowLevelCameraAnimatorActivity : AppCompatActivity() {
     mapboxMap.loadStyleUri(
       Style.MAPBOX_STREETS
     ) {
-      setupBuildings(it)
       animateCameraDelayed()
     }
   }
 
   private fun animateCameraDelayed() {
     binding.mapView.camera.apply {
-      val bearing = createBearingAnimator(cameraAnimatorOptions(0.0, 160.0)) {
-        duration = 8500
-        interpolator = AnticipateOvershootInterpolator()
+      val bearing = createBearingAnimator(cameraAnimatorOptions(-45.0)) {
+        duration = 4000
+        interpolator = AccelerateDecelerateInterpolator()
       }
       val zoom = createZoomAnimator(
-        cameraAnimatorOptions(18.0) {
-          startValue(15.0)
+        cameraAnimatorOptions(14.0) {
+          startValue(3.0)
         }
       ) {
-        duration = 5000
+        duration = 4000
         interpolator = AccelerateDecelerateInterpolator()
       }
       val pitch = createPitchAnimator(
@@ -59,36 +45,10 @@ class LowLevelCameraAnimatorActivity : AppCompatActivity() {
           startValue(0.0)
         }
       ) {
-        duration = 7000
-        interpolator = BounceInterpolator()
+        duration = 4000
+        interpolator = AccelerateDecelerateInterpolator()
       }
-      playAnimatorsTogether(bearing, zoom, pitch)
+      playAnimatorsSequentially(zoom, pitch, bearing)
     }
-  }
-
-  private fun setupBuildings(style: Style) {
-    style.getLight().position(1.5, 90.0, 80.0)
-
-    val fillExtrusionLayer = FillExtrusionLayer("3d-buildings", "composite")
-    fillExtrusionLayer.sourceLayer("building")
-    fillExtrusionLayer.filter(
-      eq(
-        get("extrude"),
-        literal("true")
-      )
-    )
-    fillExtrusionLayer.minZoom(16.5)
-    fillExtrusionLayer.fillExtrusionColor(Color.WHITE)
-    fillExtrusionLayer.fillExtrusionHeight(
-      interpolate(
-        exponential(literal(1.0)),
-        zoom(),
-        literal(16), literal(0),
-        literal(18), get("height")
-      )
-    )
-    fillExtrusionLayer.fillExtrusionBase(get(Expression.literal("min_height")))
-    fillExtrusionLayer.fillExtrusionOpacity(0.6)
-    style.addLayer(fillExtrusionLayer)
   }
 }
