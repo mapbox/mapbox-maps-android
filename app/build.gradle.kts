1658062105a20e3ffc470d14fd5f77ef84093686
Tobrun <tobrun.van.nuland@gmail.com>
2021-08-24 20:06:14 +0200

    [build] use gradle.properties configuration to enable building from s… (#580)
    * [build] use gradle.properties configuration to enable building from source, disable access token configuration when building from source
    
    * [deps] add additional ghosting of annotations dependency for building from source

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
   id("com.mapbox.maps.token")
 }
 
+val buildFromSource: String by project
+
 android {
   compileSdkVersion(AndroidVersions.compileSdkVersion)
   defaultConfig {
@@ -53,6 +55,12 @@ android {
   dexOptions {
     javaMaxHeapSize = "4g"
   }
+
+  if (buildFromSource.toBoolean()) {
+    packagingOptions {
+      pickFirst("**/libc++_shared.so")
+    }
+  }
 }
 
 dependencies {
@@ -96,5 +104,5 @@ project.apply {
   from("$rootDir/gradle/lint.gradle")
 }
 
-the<com.mapbox.AccessTokenExtension>().file =
-  "${project.rootDir}/app/src/main/res/values/developer-config.xml"
+val localPath:String = org.apache.commons.io.FilenameUtils.getFullPathNoEndSeparator(project.buildscript.sourceFile.toString())
+the<com.mapbox.AccessTokenExtension>().file = "${localPath}/src/main/res/values/developer-config.xml"
