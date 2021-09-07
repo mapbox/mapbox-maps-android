a1399c78e9996fa592a1edc28521fdfd38ba2fae
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-09-07 17:28:07 +0300

    Start creating separate example

--- app/src/main/java/com/mapbox/maps/testapp/examples/markersandcallouts/AddOneMarkerSymbolActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/markersandcallouts/AddOneMarkerSymbolActivity.kt
@@ -2,43 +2,28 @@ package com.mapbox.maps.testapp.examples.markersandcallouts
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.postDelayed
 import com.mapbox.geojson.Point
-import com.mapbox.maps.*
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 import com.mapbox.maps.extension.style.image.image
 import com.mapbox.maps.extension.style.layers.generated.symbolLayer
 import com.mapbox.maps.extension.style.layers.properties.generated.IconAnchor
 import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
 import com.mapbox.maps.extension.style.style
-import com.mapbox.maps.plugin.gestures.OnMapClickListener
-import com.mapbox.maps.plugin.gestures.addOnMapClickListener
-import com.mapbox.maps.plugin.viewannotation.ViewAnnotationPlugin
-import com.mapbox.maps.plugin.viewannotation.viewAnnotation
 import com.mapbox.maps.testapp.R
 
 /**
  * Example showing how to add a marker on map with symbol layer
  */
-class AddOneMarkerSymbolActivity : AppCompatActivity(), OnMapClickListener {
-
-  private lateinit var viewAnnotationPlugin: ViewAnnotationPlugin
-  private var count = 1
+class AddOneMarkerSymbolActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    val mapView = MapView(
-      this,
-      MapInitOptions(
-        context = this,
-        textureView = false
-      )
-    )
+    val mapView = MapView(this)
     setContentView(mapView)
 
-    viewAnnotationPlugin = mapView.viewAnnotation
     mapView.getMapboxMap().also {
       it.setCamera(
         CameraOptions.Builder()
@@ -46,11 +31,6 @@ class AddOneMarkerSymbolActivity : AppCompatActivity(), OnMapClickListener {
           .zoom(8.0)
           .build()
       )
-      // we need to wait until map is loaded to add annotations as we rely on MapOptions.size
-      // guess it should be handled in our plugin internally but leaving as is for now
-      it.addOnMapLoadedListener {
-        mapView.getMapboxMap().addOnMapClickListener(this)
-      }
     }.loadStyle(
       styleExtension = style(Style.MAPBOX_STREETS) {
         // prepare blue marker from resources
@@ -74,23 +54,5 @@ class AddOneMarkerSymbolActivity : AppCompatActivity(), OnMapClickListener {
     private const val LAYER_ID = "layer_id"
     private const val LATITUDE = 55.665957
     private const val LONGITUDE = 12.550343
-  }
-
-  override fun onMapClick(point: Point): Boolean {
-    viewAnnotationPlugin.addViewAnnotation(
-      R.layout.item_callout_view,
-      ViewAnnotationOptions.Builder()
-        .geometry(point)
-        .allowViewAnnotationsCollision(false)
-        .anchor(ViewAnnotationAnchor.TOP_RIGHT)
-        .build()
-    ) { view ->
-      view.findViewById<TextView>(R.id.textNativeView).text = "Callout ${count++}"
-      view.findViewById<ImageView>(R.id.closeNativeView).setOnClickListener {
-//        view.visibility = View.GONE
-        viewAnnotationPlugin.removeViewAnnotation(view)
-      }
-    }
-    return true
   }
 }
---
+++ app/src/main/java/com/mapbox/maps/testapp/examples/markersandcallouts/ViewAnnotationShowcase.kt
@@ -0,0 +1,96 @@
+package com.mapbox.maps.testapp.examples.markersandcallouts
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.postDelayed
+import com.mapbox.geojson.Point
+import com.mapbox.maps.*
+import com.mapbox.maps.extension.style.image.image
+import com.mapbox.maps.extension.style.layers.generated.symbolLayer
+import com.mapbox.maps.extension.style.layers.properties.generated.IconAnchor
+import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
+import com.mapbox.maps.extension.style.style
+import com.mapbox.maps.plugin.gestures.OnMapClickListener
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
+import com.mapbox.maps.plugin.viewannotation.ViewAnnotationPlugin
+import com.mapbox.maps.plugin.viewannotation.viewAnnotation
+import com.mapbox.maps.testapp.R
+
+/**
+ * Example how to add view annotations to the map.
+ *
+ * Specifically view annotations will be associated with marker icons
+ * showcasing how to implement something similar to MarkerView from Maps v9.
+ */
+class ViewAnnotationShowcase : AppCompatActivity(), OnMapClickListener {
+
+  private lateinit var viewAnnotationPlugin: ViewAnnotationPlugin
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val mapView = MapView(
+      this,
+      MapInitOptions(
+        context = this,
+        textureView = false
+      )
+    )
+    setContentView(mapView)
+
+    viewAnnotationPlugin = mapView.viewAnnotation
+    mapView.getMapboxMap().also {
+      it.setCamera(
+        CameraOptions.Builder()
+          .center(Point.fromLngLat(LONGITUDE, LATITUDE))
+          .zoom(8.0)
+          .build()
+      )
+      // we need to wait until map is loaded to add annotations as we rely on MapOptions.size
+      // guess it should be handled in our plugin internally but leaving as is for now
+      it.addOnMapLoadedListener {
+        mapView.getMapboxMap().addOnMapClickListener(this)
+      }
+    }.loadStyle(
+      styleExtension = style(Style.MAPBOX_STREETS) {
+        // prepare blue marker from resources
+        +image(BLUE_ICON_ID) {
+          bitmap(BitmapFactory.decodeResource(resources, R.drawable.blue_marker_view))
+        }
+        +geoJsonSource(SOURCE_ID) {
+          geometry(Point.fromLngLat(LONGITUDE, LATITUDE))
+        }
+        +symbolLayer(LAYER_ID, SOURCE_ID) {
+          iconImage(BLUE_ICON_ID)
+          iconAnchor(IconAnchor.BOTTOM)
+        }
+      }
+    )
+  }
+
+  override fun onMapClick(point: Point): Boolean {
+    viewAnnotationPlugin.addViewAnnotation(
+      R.layout.item_callout_view,
+      ViewAnnotationOptions.Builder()
+        .geometry(point)
+        .allowViewAnnotationsCollision(false)
+        .anchor(ViewAnnotationAnchor.TOP_RIGHT)
+        .build()
+    ) { view ->
+      view.findViewById<TextView>(R.id.textNativeView).text = "Callout ${count++}"
+      view.findViewById<ImageView>(R.id.closeNativeView).setOnClickListener {
+//        view.visibility = View.GONE
+        viewAnnotationPlugin.removeViewAnnotation(view)
+      }
+    }
+    return true
+  }
+
+  companion object {
+    private const val BLUE_ICON_ID = "blue"
+    private const val SOURCE_ID = "source_id"
+    private const val LAYER_ID = "layer_id"
+  }
+}
