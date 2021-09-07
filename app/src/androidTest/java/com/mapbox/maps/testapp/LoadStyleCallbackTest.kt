d8c1dff9a81c005a268b8b2056c9bb333f2a67df
Alexey Krasilnikov <aleksei.krasilnikov@mapbox.com>
2021-09-07 15:23:03 +0300

    #618: Add tests to reproduce the problem
    onStyleLoaded is not called in 2 of these tests.

---
+++ app/src/androidTest/java/com/mapbox/maps/testapp/LoadStyleCallbackTest.kt
@@ -0,0 +1,134 @@
+package com.mapbox.maps.testapp
+
+import android.app.Activity
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.delegates.listeners.OnMapLoadErrorListener
+import com.mapbox.maps.plugin.delegates.listeners.eventdata.MapLoadErrorType
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class LoadStyleCallbackTest {
+
+  @get:Rule
+  var testActivity = ActivityScenarioRule(TestMapActivity::class.java)
+
+  @Test
+  fun loadStyleWhileStarted() {
+    val activity = testActivity.getActivity()
+
+    if (!activity.startLatch.await(2, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+
+    val latch = CountDownLatch(1)
+    testActivity.scenario.onActivity {
+      activity.mapView.getMapboxMap().loadStyleUri(
+        styleUri = Style.LIGHT,
+        onStyleLoaded = {
+          latch.countDown()
+        },
+        onMapLoadErrorListener = object : OnMapLoadErrorListener {
+          override fun onMapLoadError(mapLoadErrorType: MapLoadErrorType, message: String) {
+            throw AssertionError("Load $mapLoadErrorType failed with $message")
+          }
+        }
+      )
+    }
+
+    if (!latch.await(10, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+  }
+
+  @Test
+  fun stopAfterLoadStyle() {
+    val activity = testActivity.getActivity()
+
+    if (!activity.startLatch.await(2, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+
+    val latch = CountDownLatch(1)
+    testActivity.scenario.onActivity {
+      activity.mapView.getMapboxMap().loadStyleUri(
+        styleUri = Style.LIGHT,
+        onStyleLoaded = {
+          latch.countDown()
+        },
+        onMapLoadErrorListener = object : OnMapLoadErrorListener {
+          override fun onMapLoadError(mapLoadErrorType: MapLoadErrorType, message: String) {
+            throw AssertionError("Load $mapLoadErrorType failed with $message")
+          }
+        }
+      )
+
+      activity.mapView.onStop()
+    }
+
+    testActivity.scenario.moveToState(Lifecycle.State.CREATED)
+
+    if (!activity.stopLatch.await(2, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+
+    testActivity.scenario.moveToState(Lifecycle.State.RESUMED)
+
+    if (!latch.await(10, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+  }
+
+  @Test
+  fun loadStyleWhileStopped() {
+    val activity = testActivity.getActivity()
+
+    if (!activity.startLatch.await(2, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+
+    testActivity.scenario.moveToState(Lifecycle.State.CREATED)
+
+    if (!activity.stopLatch.await(2, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+
+    val latch = CountDownLatch(1)
+    testActivity.scenario.onActivity {
+      activity.mapView.getMapboxMap().loadStyleUri(
+        styleUri = Style.LIGHT,
+        onStyleLoaded = {
+          latch.countDown()
+        },
+        onMapLoadErrorListener = object : OnMapLoadErrorListener {
+          override fun onMapLoadError(mapLoadErrorType: MapLoadErrorType, message: String) {
+            throw AssertionError("Load $mapLoadErrorType failed with $message")
+          }
+        }
+      )
+    }
+
+    Thread.sleep(100)
+
+    testActivity.scenario.moveToState(Lifecycle.State.RESUMED)
+
+    if (!latch.await(10, TimeUnit.SECONDS)) {
+      throw TimeoutException()
+    }
+  }
+
+  private fun <T : Activity> ActivityScenarioRule<T>.getActivity(): T {
+    var activity: T? = null
+    scenario.onActivity { activity = it }
+    return activity!!
+  }
+}
