c17a023363f561f087cd3332a63dbba0b0af4ccc
Konstantin Käfer <mail@kkaefer.com>
2021-09-30 23:32:39 +0200

    Use correct enum for setting render thread priority

--- sdk/src/main/java/com/mapbox/maps/renderer/WorkerHandlerThread.kt
+++ sdk/src/main/java/com/mapbox/maps/renderer/WorkerHandlerThread.kt
@@ -2,6 +2,7 @@ package com.mapbox.maps.renderer
 
 import android.os.Handler
 import android.os.HandlerThread
+import android.os.Process
 import androidx.annotation.VisibleForTesting
 import com.mapbox.common.Logger
 
@@ -23,7 +24,7 @@ internal class WorkerHandlerThread {
 
   fun start() {
     handlerThread = HandlerThread(HANDLE_THREAD_NAME).apply {
-      priority = Thread.MAX_PRIORITY
+      priority = Process.THREAD_PRIORITY_DISPLAY
       start()
       handler = Handler(this.looper)
     }
