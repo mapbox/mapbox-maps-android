9994eba7058a5c35d356ad576d6914d6570ad296
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-10-04 14:32:38 +0300

    Fix wrong attribute reference when token is missing

--- sdk/src/main/java/com/mapbox/maps/MapboxConfigurationException.kt
+++ sdk/src/main/java/com/mapbox/maps/MapboxConfigurationException.kt
@@ -11,7 +11,7 @@ class MapboxConfigurationException : RuntimeException {
   constructor() : super(
     """
       Using MapView requires providing a valid access token when inflating or creating the view.
-      Provide the token by creating a $MAPBOX_ACCESS_TOKEN_RESOURCE_NAME string resource or by passing it using MapView's 'mapbox_accessToken' XML attribute.
+      Provide the token by creating a $MAPBOX_ACCESS_TOKEN_RESOURCE_NAME string resource or by passing it using MapView's 'mapbox_resourcesAccessToken' XML attribute.
       The access token parameter is required when using a Mapbox service.
       Please see https://www.mapbox.com/help/create-api-access-token/ to learn how to create one.
       More information in this guide https://www.mapbox.com/help/first-steps-android-sdk/#access-tokens.
