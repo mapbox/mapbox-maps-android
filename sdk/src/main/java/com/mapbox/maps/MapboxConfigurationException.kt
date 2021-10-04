ac183ddc065668f099c441f454db59720b673142
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-10-04 14:48:59 +0300

    Update sdk/src/main/java/com/mapbox/maps/MapboxConfigurationException.kt
    Co-authored-by: Peng Liu <peng.liu@mapbox.com>

--- sdk/src/main/java/com/mapbox/maps/MapboxConfigurationException.kt
+++ sdk/src/main/java/com/mapbox/maps/MapboxConfigurationException.kt
@@ -13,8 +13,8 @@ class MapboxConfigurationException : RuntimeException {
       Using MapView requires providing a valid access token when inflating or creating the view.
       Provide the token by either:
         1. Initialising the MapView programmatically using 'MapInitOptions' and configure the token using 'MapInitOptions.resourceOptions.accessToken'.
-        2. Or creating a $MAPBOX_ACCESS_TOKEN_RESOURCE_NAME string resource.
-        3. Or by passing it using MapView's 'mapbox_resourcesAccessToken' XML attribute.
+        2. Or by passing it using MapView's 'mapbox_resourcesAccessToken' XML attribute.
+        3. Or by creating a $MAPBOX_ACCESS_TOKEN_RESOURCE_NAME string resource.
       The access token parameter is required when using a Mapbox service.
       Please see https://www.mapbox.com/help/create-api-access-token/ to learn how to create one.
       More information in this guide https://www.mapbox.com/help/first-steps-android-sdk/#access-tokens.
