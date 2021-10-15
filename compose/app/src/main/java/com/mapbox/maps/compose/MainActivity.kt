85c5040d312fde5bf5072e1118cef6d82ca3131e
Alexander Kulikovskiy <aleksandr.kulikovskiy@mapbox.com>
2021-10-15 17:28:20 +0300

    refactor main activity. remove lifecycle observer. remove remembering mapView

--- compose/app/src/main/java/com/mapbox/maps/compose/MainActivity.kt
+++ compose/app/src/main/java/com/mapbox/maps/compose/MainActivity.kt
@@ -1,23 +1,21 @@
 package com.mapbox.maps.compose
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+
+private const val LATITUDE = 60.239
+private const val LONGITUDE = 25.004
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,60 +27,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MapboxMap() {
-    val mapView = rememberMapViewWithLifeCycle()
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        AndroidView(
-            { mapView }
-        ) { mapView ->
-            mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
-        }
+private fun MapboxMap() {
+    val mapView = MapView()
+    AndroidView(
+        factory = { mapView },
+        modifier =  Modifier.fillMaxSize()
+    ) { mapView ->
+        mapView.getMapboxMap()
+          .apply {
+            loadStyleUri(Style.MAPBOX_STREETS)
+            setCamera(
+              CameraOptions.Builder()
+                .center(Point.fromLngLat(LONGITUDE, LATITUDE))
+                .zoom(9.0)
+                .build()
+            )
+          }
     }
+}
+
+@Composable
+private fun MapView(): MapView {
+    val context = LocalContext.current
+    return MapView(context)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+private fun DefaultPreview() {
     MapboxMap()
 }
-
-@SuppressLint("ResourceType")
-@Composable
-fun rememberMapViewWithLifeCycle(): MapView {
-    val context = LocalContext.current
-
-    val mapView = remember {
-        MapView(context)
-    }
-    val lifeCycleObserver = rememberMapLifecycleObserver(mapView)
-    val lifeCycle = LocalLifecycleOwner.current.lifecycle
-    DisposableEffect(lifeCycle) {
-        lifeCycle.addObserver(lifeCycleObserver)
-        onDispose {
-            lifeCycle.removeObserver(lifeCycleObserver)
-        }
-    }
-
-    return mapView
-}
-
-@SuppressLint("Lifecycle")
-@Composable
-fun rememberMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
-    remember(mapView) {
-        LifecycleEventObserver { _, event ->
-            when (event) {
-                Lifecycle.Event.ON_CREATE,
-                Lifecycle.Event.ON_RESUME,
-                Lifecycle.Event.ON_PAUSE -> {}
-                Lifecycle.Event.ON_START -> mapView.onStart()
-                Lifecycle.Event.ON_STOP -> mapView.onStop()
-                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
-                else -> throw IllegalStateException()
-            }
-        }
-    }
