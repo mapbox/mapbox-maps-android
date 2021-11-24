814f8a3764401b5fa66a8c4451947b94c3d6f5c1
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2021-11-24 18:06:16 +0200

    View annotations: add instrument tests (#894)
    * Add instrument tests
    
    * Add test for automatic view visibility handling
    
    * PR fixes
    
    * PR fixes
    
    * Refactor test setup

---
+++ sdk/src/androidTest/java/com/mapbox/maps/TestUtils.kt
@@ -0,0 +1,19 @@
+package com.mapbox.maps
+
+import android.view.View
+import androidx.core.view.children
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
+
+internal fun MapView.hasChildView(view: View) = children.any { it == view }
+
+internal fun MapView.getChildViewIndex(view: View) = children.indexOf(view)
+
+internal fun CountDownLatch.throwExceptionOnTimeoutMs(timeoutMs: Long = DEFAULT_LATCH_TIMEOUT_MS) {
+  if (!await(timeoutMs, TimeUnit.MILLISECONDS)) {
+    throw TimeoutException()
+  }
+}
+
+internal const val DEFAULT_LATCH_TIMEOUT_MS = 5_000L
