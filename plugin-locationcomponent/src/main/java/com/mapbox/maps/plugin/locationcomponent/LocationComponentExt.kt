bd11f1a352dd4bcb99dd783cf85fa48252927258
Kevin Li <kevin.li@mapbox.com>
2022-01-17 22:12:19 +0800

    polish codes for LocationComponentPlugin.createDefault2DPuck (#1060)

--- plugin-locationcomponent/src/main/java/com/mapbox/maps/plugin/locationcomponent/LocationComponentExt.kt
+++ plugin-locationcomponent/src/main/java/com/mapbox/maps/plugin/locationcomponent/LocationComponentExt.kt
@@ -3,6 +3,7 @@
 package com.mapbox.maps.plugin.locationcomponent
 
 import android.content.Context
+import androidx.annotation.DrawableRes
 import androidx.core.content.res.ResourcesCompat
 import com.mapbox.maps.plugin.LocationPuck2D
 import com.mapbox.maps.plugin.Plugin
@@ -15,6 +16,12 @@ val MapPluginProviderDelegate.location: LocationComponentPlugin
   @JvmName("getLocationComponent")
   get() = this.getPlugin(Plugin.MAPBOX_LOCATION_COMPONENT_PLUGIN_ID)!!
 
+private fun Context.getCompatDrawable(@DrawableRes resId: Int) = ResourcesCompat.getDrawable(
+  this.resources,
+  resId,
+  null
+)
+
 /**
  * Create a [LocationPuck2D] instance with or without an arrow bearing image.
  * @param context the context of application
@@ -24,38 +31,18 @@ val MapPluginProviderDelegate.location: LocationComponentPlugin
 fun LocationComponentPlugin.createDefault2DPuck(
   context: Context,
   withBearing: Boolean = false
-): LocationPuck2D {
-  if (withBearing) {
-    return LocationPuck2D(
-      topImage = ResourcesCompat.getDrawable(context.resources, R.drawable.mapbox_user_icon, null),
-      bearingImage = ResourcesCompat.getDrawable(
-        context.resources,
-        R.drawable.mapbox_user_bearing_icon,
-        null
-      ),
-      shadowImage = ResourcesCompat.getDrawable(
-        context.resources,
-        R.drawable.mapbox_user_stroke_icon,
-        null
-      )
-    )
-  } else {
-    return LocationPuck2D(
-      topImage = ResourcesCompat.getDrawable(
-        context.resources,
-        R.drawable.mapbox_user_icon,
-        null
-      ),
-      bearingImage = ResourcesCompat.getDrawable(
-        context.resources,
-        R.drawable.mapbox_user_stroke_icon,
-        null
-      ),
-      shadowImage = ResourcesCompat.getDrawable(
-        context.resources,
-        R.drawable.mapbox_user_icon_shadow,
-        null
-      )
-    )
-  }
-}
+): LocationPuck2D = LocationPuck2D(
+  topImage = context.getCompatDrawable(R.drawable.mapbox_user_icon),
+  bearingImage = context.getCompatDrawable(
+    if (withBearing)
+      R.drawable.mapbox_user_bearing_icon
+    else
+      R.drawable.mapbox_user_stroke_icon
+  ),
+  shadowImage = context.getCompatDrawable(
+    if (withBearing)
+      R.drawable.mapbox_user_stroke_icon
+    else
+      R.drawable.mapbox_user_icon_shadow
+  )
+)
