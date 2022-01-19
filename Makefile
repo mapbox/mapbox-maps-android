e76ca7f39c6150cadbd21359317559db21fae373
Kevin Li <kevin.li@mapbox.com>
2022-01-19 15:19:17 +0800

    Add job to check changelog

--- Makefile
+++ Makefile
@@ -32,6 +32,12 @@ build:
 checkApi:
 	./scripts/java-api-check-all.sh "$(TAG)"
 
+.PHONY: checkChangelog
+checkChangelog:
+	unset GITHUB_TOKEN; \
+	echo "$(shell mbx-ci github reader token)" > gh_token.txt;\
+	sh scripts/check-changelog.sh
+
 .PHONY: proguard
 proguard:
 	./gradlew clean;
---
+++ scripts/check-changelog.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -Eeuxo pipefail
+
+if [ $(git rev-parse origin/main) == $(git rev-parse HEAD) ]; then
+    echo "No new changes, skipping changelog check."
+    exit 0
+fi
+
+if ! command -v gh &> /dev/null
+then
+    echo "gh (github cli tool) is not found, install it first"
+    exit
+fi
+
+gh auth login --with-token < ./gh_token.txt
+
+pr_number=${CIRCLE_PULL_REQUEST:51}
+echo "$pr_number"
+
+skip_changelog_prs=$(gh pr list --label "skip changelog")
+if grep -q "$pr_number" <<< "$skip_changelog_prs"; then
+  echo "$pr_number has skip changelog label"
+  exit 0
+fi
+
+changelog_diff=$(git diff origin/main..HEAD CHANGELOG.md)
+
+if [[ -z "$changelog_diff" ]]; then
+    echo "No changes in CHANGELOG.md found"
+    exit 1
+fi
+
+echo "All good. You have modified CHANGELOG.md"
