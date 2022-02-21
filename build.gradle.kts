255bb5a0824ebe132714b447f811ebc2626c16e0
ank27 <ankur.khandelwal@mapbox.com>
2022-02-21 21:38:41 +0200

    publish_android_snapshot

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,6 +42,22 @@ allprojects {
         create<BasicAuthentication>("basic")
       }
     }
+
+    maven {
+      url = uri("https://api.mapbox.com/downloads/v2/snapshots/maven")
+      authentication {
+        create<BasicAuthentication>("basic")
+      }
+      credentials {
+        // Do not change the username below.
+        // This should always be `mapbox` (not your username).
+        username = "mapbox"
+        // Use the secret token you stored in gradle.properties as the password
+        password =
+          System.getenv("SDK_REGISTRY_TOKEN") ?: project.property("SDK_REGISTRY_TOKEN") as String
+      }
+    }
+
     maven {
       url = uri("https://oss.jfrog.org/artifactory/oss-snapshot-local/")
     }
