5c0f6d34488ddcefbf4286b13f2803cd6f36be2e
ank27 <ankur.khandelwal@mapbox.com>
2022-03-28 17:42:11 +0300

    bump common deps

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -87,7 +87,7 @@ object Versions {
   const val mapboxJavaServices = "5.4.1"
   const val mapboxBase = "0.5.0"
   const val mapboxGlNative = "10.4.1"
-  const val mapboxCommon = "21.3.0-beta.1"
+  const val mapboxCommon = "21.3.0-beta.2"
   const val mapboxAndroidCore = "5.0.0"
   const val mapboxAndroidTelemetry = "8.1.0"
   const val androidxCore = "1.3.1"
