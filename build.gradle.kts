049d5a44a703adaf3b4d6d85a1ad6c04d74bb3b7
Peng Liu <peng.liu@mapbox.com>
2022-03-29 20:40:54 +0300

    Enable publish Android Auto separately from the main sdk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ buildscript {
   repositories {
     google()
     mavenCentral()
+    mavenLocal()
     maven {
       url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
       credentials {
@@ -52,6 +53,10 @@ allprojects {
 plugins {
   id(Plugins.dokkaId) version Versions.pluginDokka
   id(Plugins.binaryCompatibilityValidatorId) version Versions.pluginBinaryCompatibilityValidator
+  // Used to print dependency tree of the task, useful to debug gradle tasks
+  // Ticket to track adding this feature to gradle officially: https://github.com/gradle/gradle/issues/980
+  id(Plugins.taskTreeId) version Versions.pluginTaskTree
+
 }
 repositories {
   maven(url = "https://dl.bintray.com/kotlin/dokka")
