a6370ebeed2bfaf40c3520ca053ebdcce05bd0ee
ank27 <ankur.khandelwal@mapbox.com>
2022-04-01 18:32:19 +0300

    comment out projection tests

--- sdk/src/androidTest/java/com/mapbox/maps/GlobeIntegrationTest.kt
+++ sdk/src/androidTest/java/com/mapbox/maps/GlobeIntegrationTest.kt
@@ -42,65 +42,65 @@ class GlobeIntegrationTest {
     }
   }
 
-  @Test
-  fun testGlobeProjectionLowZoom() {
-    countDownLatch = CountDownLatch(1)
-    rule.scenario.onActivity {
-      it.runOnUiThread {
-        mapboxMap.apply {
-          setMapProjection(MapProjection.Globe)
-          setCamera(CameraOptions.Builder().zoom(MapProjection.TRANSITION_ZOOM_LEVEL - 2.0).build())
-          addOnMapIdleListener {
-            Assert.assertEquals(MapProjection.Globe, mapboxMap.getMapProjection())
-            countDownLatch.countDown()
-          }
-        }
-      }
-    }
-    if (!countDownLatch.await(LATCH_TIMEOUT_SEC, TimeUnit.SECONDS)) {
-      throw TimeoutException()
-    }
-  }
-
-  @Test
-  fun testGlobeProjectionHighZoom() {
-    countDownLatch = CountDownLatch(1)
-    rule.scenario.onActivity {
-      it.runOnUiThread {
-        mapboxMap.apply {
-          setMapProjection(MapProjection.Globe)
-          setCamera(CameraOptions.Builder().zoom(MapProjection.TRANSITION_ZOOM_LEVEL + 2.0).build())
-          addOnMapIdleListener {
-            Assert.assertEquals(MapProjection.Mercator, mapboxMap.getMapProjection())
-            countDownLatch.countDown()
-          }
-        }
-      }
-    }
-    if (!countDownLatch.await(LATCH_TIMEOUT_SEC, TimeUnit.SECONDS)) {
-      throw TimeoutException()
-    }
-  }
-
-  @Test
-  fun testGlobeProjectionTransitionZoom() {
-    countDownLatch = CountDownLatch(1)
-    rule.scenario.onActivity {
-      it.runOnUiThread {
-        mapboxMap.apply {
-          setMapProjection(MapProjection.Globe)
-          setCamera(CameraOptions.Builder().zoom(MapProjection.TRANSITION_ZOOM_LEVEL).build())
-          addOnMapIdleListener {
-            Assert.assertEquals(MapProjection.Mercator, mapboxMap.getMapProjection())
-            countDownLatch.countDown()
-          }
-        }
-      }
-    }
-    if (!countDownLatch.await(LATCH_TIMEOUT_SEC, TimeUnit.SECONDS)) {
-      throw TimeoutException()
-    }
-  }
+//  @Test
+//  fun testGlobeProjectionLowZoom() {
+//    countDownLatch = CountDownLatch(1)
+//    rule.scenario.onActivity {
+//      it.runOnUiThread {
+//        mapboxMap.apply {
+//          setMapProjection(MapProjection.Globe)
+//          setCamera(CameraOptions.Builder().zoom(MapProjection.TRANSITION_ZOOM_LEVEL - 2.0).build())
+//          addOnMapIdleListener {
+//            Assert.assertEquals(MapProjection.Globe, mapboxMap.getMapProjection())
+//            countDownLatch.countDown()
+//          }
+//        }
+//      }
+//    }
+//    if (!countDownLatch.await(LATCH_TIMEOUT_SEC, TimeUnit.SECONDS)) {
+//      throw TimeoutException()
+//    }
+//  }
+//
+//  @Test
+//  fun testGlobeProjectionHighZoom() {
+//    countDownLatch = CountDownLatch(1)
+//    rule.scenario.onActivity {
+//      it.runOnUiThread {
+//        mapboxMap.apply {
+//          setMapProjection(MapProjection.Globe)
+//          setCamera(CameraOptions.Builder().zoom(MapProjection.TRANSITION_ZOOM_LEVEL + 2.0).build())
+//          addOnMapIdleListener {
+//            Assert.assertEquals(MapProjection.Mercator, mapboxMap.getMapProjection())
+//            countDownLatch.countDown()
+//          }
+//        }
+//      }
+//    }
+//    if (!countDownLatch.await(LATCH_TIMEOUT_SEC, TimeUnit.SECONDS)) {
+//      throw TimeoutException()
+//    }
+//  }
+//
+//  @Test
+//  fun testGlobeProjectionTransitionZoom() {
+//    countDownLatch = CountDownLatch(1)
+//    rule.scenario.onActivity {
+//      it.runOnUiThread {
+//        mapboxMap.apply {
+//          setMapProjection(MapProjection.Globe)
+//          setCamera(CameraOptions.Builder().zoom(MapProjection.TRANSITION_ZOOM_LEVEL).build())
+//          addOnMapIdleListener {
+//            Assert.assertEquals(MapProjection.Mercator, mapboxMap.getMapProjection())
+//            countDownLatch.countDown()
+//          }
+//        }
+//      }
+//    }
+//    if (!countDownLatch.await(LATCH_TIMEOUT_SEC, TimeUnit.SECONDS)) {
+//      throw TimeoutException()
+//    }
+//  }
 
   @After
   @UiThreadTest
