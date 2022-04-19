9ad1fd22461a4547e25188e1a685ab1bc37114b8
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2022-04-19 11:09:48 +0300

    Add docs to an interface

--- sdk/src/main/java/com/mapbox/maps/renderer/MapboxRendererInterface.kt
+++ sdk/src/main/java/com/mapbox/maps/renderer/MapboxRendererInterface.kt
@@ -7,21 +7,48 @@ import com.mapbox.maps.MapView
 
 internal interface MapboxRendererInterface {
 
+  /**
+   * Triggered when Activity.onStart or Fragment.onStart is called by Android.
+   */
   fun onStart()
 
+  /**
+   * Triggered when Activity.onStop or Fragment.onStop is called by Android.
+   */
   fun onStop()
 
+  /**
+   * Triggered when Activity.onDestroy or Fragment.onDestroy is called by Android.
+   */
   fun onDestroy()
 
+  /**
+   * Queue user's event on the render thread. Runnable will be executed and buffers will be swapped afterwards.
+   */
   fun queueRenderEvent(runnable: Runnable)
 
+  /**
+   * Queue user's event on the render thread. Runnable will be executed but buffers will not be swapped afterwards.
+   */
   fun queueEvent(runnable: Runnable)
 
+  /**
+   * Take current map snapshot by calling glReadPixels. This method is synchronous.
+   */
   fun snapshot(): Bitmap?
 
+  /**
+   * Take current map snapshot by calling glReadPixels. This method is asynchronous.
+   */
   fun snapshot(listener: MapView.OnSnapshotReady)
 
+  /**
+   * Set maximum FPS limit we want to render with.
+   */
   fun setMaximumFps(fps: Int)
 
+  /**
+   * Set FPS change callback listener.
+   */
   fun setOnFpsChangedListener(listener: OnFpsChangedListener)
 }
