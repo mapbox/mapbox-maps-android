cf9b70ae9e8003924b65865737ec8aae2c8175a4
yunik <dmitry.yunitski@mapbox.com>
2022-04-29 13:33:21 +0300

    Bump gl-native 10.5.0

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -86,7 +86,7 @@ object Versions {
   const val mapboxGestures = "0.7.0"
   const val mapboxJavaServices = "5.4.1"
   const val mapboxBase = "0.5.0"
-  const val mapboxGlNative = "10.5.0-rc.1"
+  const val mapboxGlNative = "10.5.0"
   const val mapboxCommon = "21.3.0-rc.2"
   const val mapboxAndroidCore = "5.0.0"
   const val mapboxAndroidTelemetry = "8.1.0"
