40448d99b30a0adde29f1b615ad976c1d5bd5e7f
Peng Liu <peng.liu@mapbox.com>
2022-05-20 13:19:04 +0300

    Use data driven styling to add 2 models in the same model layer.

--- app/src/main/java/com/mapbox/maps/testapp/examples/terrain3D/ModelLayerActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/terrain3D/ModelLayerActivity.kt
@@ -2,15 +2,19 @@ package com.mapbox.maps.testapp.examples.terrain3D
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
 import com.mapbox.maps.dsl.cameraOptions
+import com.mapbox.maps.extension.style.expressions.dsl.generated.get
 import com.mapbox.maps.extension.style.layers.generated.modelLayer
 import com.mapbox.maps.extension.style.layers.properties.generated.ModelType
 import com.mapbox.maps.extension.style.model.model
 import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
 import com.mapbox.maps.extension.style.style
+import com.mapbox.turf.TurfMeasurement
 
 /**
  * Showcase adding a 3D model using model layer.
@@ -26,36 +30,49 @@ class ModelLayerActivity : AppCompatActivity() {
     mapView.getMapboxMap().apply {
       setCamera(
         cameraOptions {
-          center(HELSINKI)
-          zoom(15.0)
+          center(TurfMeasurement.midpoint(HELSINKI, MAPBOX_HELSINKI))
+          zoom(14.0)
           pitch(45.0)
         }
       )
       loadStyle(
         style(Style.LIGHT) {
-          +model(MODEL_ID) {
-            uri(SAMPLE_MODEL_URI)
+          +model(MODEL_ID_1) {
+            uri(SAMPLE_MODEL_URI_1)
+          }
+          +model(MODEL_ID_2) {
+            uri(SAMPLE_MODEL_URI_2)
           }
           +geoJsonSource(SOURCE_ID) {
-            geometry(HELSINKI)
+            featureCollection(
+              FeatureCollection.fromFeatures(
+                listOf(
+                  Feature.fromGeometry(HELSINKI).also { it.addStringProperty(MODEL_ID_KEY, MODEL_ID_1) },
+                  Feature.fromGeometry(MAPBOX_HELSINKI).also { it.addStringProperty(MODEL_ID_KEY, MODEL_ID_2) }
+                )
+              )
+            )
           }
           +modelLayer(MODEL_LAYER_ID, SOURCE_ID) {
-            modelId(MODEL_ID)
+            modelId(get(MODEL_ID_KEY))
             modelType(ModelType.COMMON_3D)
             modelScale(listOf(100.0, 100.0, 100.0))
           }
         }
       )
     }
-
   }
 
   private companion object {
     const val SOURCE_ID = "source-id"
     const val MODEL_LAYER_ID = "model-layer-id"
-    const val MODEL_ID = "model-id"
-    const val SAMPLE_MODEL_URI =
+    const val MODEL_ID_KEY = "model-id-key"
+    const val MODEL_ID_1 = "model-id-1"
+    const val MODEL_ID_2 = "model-id-2"
+    const val SAMPLE_MODEL_URI_1 =
       "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF-Embedded/Duck.gltf"
+    const val SAMPLE_MODEL_URI_2 = "asset://sportcar.glb"
     val HELSINKI = Point.fromLngLat(24.9384, 60.1699)
+    val MAPBOX_HELSINKI = Point.fromLngLat(24.945389069265598, 60.17195694011002)
   }
 }
