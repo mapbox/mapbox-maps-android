2d10e751617fe0f3662bd308322a38e46a5a44f5
Peng Liu <peng.liu@mapbox.com>
2022-06-03 17:11:28 +0300

    Fix ktlint

--- compose-app/src/main/java/com/mapbox/maps/compose/ui/theme/Theme.kt
+++ compose-app/src/main/java/com/mapbox/maps/compose/ui/theme/Theme.kt
@@ -7,32 +7,32 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+  primary = Purple200,
+  primaryVariant = Purple700,
+  secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+  primary = Purple500,
+  primaryVariant = Purple700,
+  secondary = Teal200
 )
 
 @Composable
 fun TestMapboxMapComposeTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+  darkTheme: Boolean = isSystemInDarkTheme(),
+  content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+  val colors = if (darkTheme) {
+    DarkColorPalette
+  } else {
+    LightColorPalette
+  }
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+  MaterialTheme(
+    colors = colors,
+    typography = Typography,
+    shapes = Shapes,
+    content = content
+  )
 }
