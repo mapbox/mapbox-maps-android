d4da047da1e04b415df015da49a779d4cec39dcc
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2022-06-22 17:21:32 +0300

    Align more examples

--- app/src/main/java/com/mapbox/maps/testapp/examples/camera/LowLevelCameraAnimatorActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/camera/LowLevelCameraAnimatorActivity.kt
@@ -3,52 +3,47 @@ package com.mapbox.maps.testapp.examples.camera
 import android.os.Bundle
 import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.maps.MapView
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.plugin.animation.CameraAnimatorOptions.Companion.cameraAnimatorOptions
 import com.mapbox.maps.plugin.animation.camera
-import com.mapbox.maps.testapp.databinding.ActivityCameraAnimateBinding
 
 class LowLevelCameraAnimatorActivity : AppCompatActivity() {
 
   private lateinit var mapboxMap: MapboxMap
-  private lateinit var binding: ActivityCameraAnimateBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    binding = ActivityCameraAnimateBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-    mapboxMap = binding.mapView.getMapboxMap()
+    val mapView = MapView(this)
+    setContentView(mapView)
+    mapboxMap = mapView.getMapboxMap()
     mapboxMap.loadStyleUri(
       Style.MAPBOX_STREETS
     ) {
-      animateCameraDelayed()
-    }
-  }
-
-  private fun animateCameraDelayed() {
-    binding.mapView.camera.apply {
-      val bearing = createBearingAnimator(cameraAnimatorOptions(-45.0)) {
-        duration = 4000
-        interpolator = AccelerateDecelerateInterpolator()
-      }
-      val zoom = createZoomAnimator(
-        cameraAnimatorOptions(14.0) {
-          startValue(3.0)
+      mapView.camera.apply {
+        val bearing = createBearingAnimator(cameraAnimatorOptions(-45.0)) {
+          duration = 4000
+          interpolator = AccelerateDecelerateInterpolator()
         }
-      ) {
-        duration = 4000
-        interpolator = AccelerateDecelerateInterpolator()
-      }
-      val pitch = createPitchAnimator(
-        cameraAnimatorOptions(55.0) {
-          startValue(0.0)
+        val zoom = createZoomAnimator(
+          cameraAnimatorOptions(14.0) {
+            startValue(3.0)
+          }
+        ) {
+          duration = 4000
+          interpolator = AccelerateDecelerateInterpolator()
         }
-      ) {
-        duration = 4000
-        interpolator = AccelerateDecelerateInterpolator()
+        val pitch = createPitchAnimator(
+          cameraAnimatorOptions(55.0) {
+            startValue(0.0)
+          }
+        ) {
+          duration = 4000
+          interpolator = AccelerateDecelerateInterpolator()
+        }
+        playAnimatorsSequentially(zoom, pitch, bearing)
       }
-      playAnimatorsSequentially(zoom, pitch, bearing)
     }
   }
 }
