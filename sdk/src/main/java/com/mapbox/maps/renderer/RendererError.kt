62c3c4dd3254d00c24cffac68903782be5ad0e67
Peng Liu <peng.liu@mapbox.com>
2022-06-23 17:29:03 +0300

    Add missing API doc for eglErrorCode.

--- sdk/src/main/java/com/mapbox/maps/renderer/RendererError.kt
+++ sdk/src/main/java/com/mapbox/maps/renderer/RendererError.kt
@@ -7,7 +7,12 @@ import javax.microedition.khronos.egl.EGL10
  * Typed enum wrapping up all the EGL error codes represented as ints.
  * We do have SDK specific error types: [NO_VALID_EGL_CONFIG_FOUND] and [OUT_OF_MEMORY].
  */
-class RendererError(val eglErrorCode: Int) {
+class RendererError(
+  /**
+   * EGL error code.
+   */
+  val eglErrorCode: Int
+) {
   /**
    * Indicates whether some other object is "equal to" this one.
    */
@@ -37,6 +42,7 @@ class RendererError(val eglErrorCode: Int) {
      */
     @JvmField
     val NO_VALID_EGL_CONFIG_FOUND = RendererError(0)
+
     /**
      * Represents out of memory caused by [EGL10.EGL_BAD_ALLOC].
      *
