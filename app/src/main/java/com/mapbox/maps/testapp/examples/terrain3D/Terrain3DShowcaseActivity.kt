3b3778fd37946d8f6af4f549025eac62be2fdc35
ank27 <ankur.khandelwal@mapbox.com>
2022-07-08 11:15:17 +0300

    test viewannotation with terrain

--- app/src/main/java/com/mapbox/maps/testapp/examples/terrain3D/Terrain3DShowcaseActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/terrain3D/Terrain3DShowcaseActivity.kt
@@ -1,9 +1,14 @@
 package com.mapbox.maps.testapp.examples.terrain3D
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.geojson.Point
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
+import com.mapbox.maps.dsl.cameraOptions
 import com.mapbox.maps.extension.style.atmosphere.generated.atmosphere
 import com.mapbox.maps.extension.style.layers.generated.skyLayer
 import com.mapbox.maps.extension.style.layers.properties.generated.ProjectionName
@@ -12,7 +17,14 @@ import com.mapbox.maps.extension.style.projection.generated.projection
 import com.mapbox.maps.extension.style.sources.generated.rasterDemSource
 import com.mapbox.maps.extension.style.style
 import com.mapbox.maps.extension.style.terrain.generated.terrain
+import com.mapbox.maps.plugin.gestures.OnMapClickListener
+import com.mapbox.maps.plugin.gestures.addOnMapClickListener
+import com.mapbox.maps.testapp.R
 import com.mapbox.maps.testapp.databinding.ActivityTerrainShowcaseBinding
+import com.mapbox.maps.testapp.databinding.ItemCalloutViewBinding
+import com.mapbox.maps.testapp.examples.markersandcallouts.ViewAnnotationBasicAddActivity
+import com.mapbox.maps.viewannotation.ViewAnnotationManager
+import com.mapbox.maps.viewannotation.viewAnnotationOptions
 
 /**
  * Example that demonstrates realistic map with 3D terrain and atmosphere sky layer.
@@ -20,12 +32,13 @@ import com.mapbox.maps.testapp.databinding.ActivityTerrainShowcaseBinding
 class Terrain3DShowcaseActivity : AppCompatActivity() {
 
   private lateinit var mapboxMap: MapboxMap
-
+  private lateinit var viewAnnotationManager: ViewAnnotationManager
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     val binding = ActivityTerrainShowcaseBinding.inflate(layoutInflater)
     setContentView(binding.root)
     mapboxMap = binding.mapView.getMapboxMap()
+    viewAnnotationManager = binding.mapView.viewAnnotationManager
     mapboxMap.loadStyle(
       styleExtension = style(Style.SATELLITE_STREETS) {
         +rasterDemSource(SOURCE) {
@@ -38,15 +51,55 @@ class Terrain3DShowcaseActivity : AppCompatActivity() {
           skyType(SkyType.ATMOSPHERE)
           skyAtmosphereSun(listOf(-50.0, 90.2))
         }
-        +atmosphere { }
+        +atmosphere {
+          color("#0F0F0F")
+        }
         +projection(ProjectionName.GLOBE)
       }
     )
+    addViewAnnotation(CAMERA_CENTER)
+  }
+
+  @SuppressLint("SetTextI18n")
+  private fun addViewAnnotation(point: Point) {
+    val viewAnnotation = viewAnnotationManager.addViewAnnotation(
+      resId = R.layout.item_callout_view,
+      options = viewAnnotationOptions {
+        geometry(point)
+        allowOverlap(true)
+      }
+    )
+    ItemCalloutViewBinding.bind(viewAnnotation).apply {
+      textNativeView.text = "lat=%.2f\nlon=%.2f".format(point.latitude(), point.longitude())
+      closeNativeView.setOnClickListener {
+        viewAnnotationManager.removeViewAnnotation(viewAnnotation)
+      }
+      selectButton.setOnClickListener { b ->
+        val button = b as Button
+        val isSelected = button.text.toString().equals("SELECT", true)
+        val pxDelta = if (isSelected) SELECTED_ADD_COEF_PX else -SELECTED_ADD_COEF_PX
+        button.text = if (isSelected) "DESELECT" else "SELECT"
+        viewAnnotationManager.updateViewAnnotation(
+          viewAnnotation,
+          viewAnnotationOptions {
+            selected(isSelected)
+          }
+        )
+        (button.layoutParams as ViewGroup.MarginLayoutParams).apply {
+          bottomMargin += pxDelta
+          rightMargin += pxDelta
+          leftMargin += pxDelta
+        }
+        button.requestLayout()
+      }
+    }
   }
 
   companion object {
+    const val SELECTED_ADD_COEF_PX = 25
     private const val SOURCE = "TERRAIN_SOURCE"
     private const val SKY_LAYER = "sky"
     private const val TERRAIN_URL_TILE_RESOURCE = "mapbox://mapbox.mapbox-terrain-dem-v1"
+    private val CAMERA_CENTER = Point.fromLngLat(138.7274, 35.3606)
   }
 }
