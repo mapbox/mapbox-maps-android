208833b5128a423af1c597f05b51d9c5e3ced528
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2022-08-16 11:38:51 +0300

    Change list to a set

--- sdk/src/main/java/com/mapbox/maps/renderer/MapboxWidgetRenderer.kt
+++ sdk/src/main/java/com/mapbox/maps/renderer/MapboxWidgetRenderer.kt
@@ -4,7 +4,7 @@ import android.opengl.GLES20
 import com.mapbox.maps.logE
 import com.mapbox.maps.renderer.egl.EGLCore
 import com.mapbox.maps.renderer.widget.Widget
-import java.util.concurrent.CopyOnWriteArrayList
+import java.util.concurrent.CopyOnWriteArraySet
 import javax.microedition.khronos.egl.EGLContext
 import javax.microedition.khronos.egl.EGLSurface
 
@@ -19,7 +19,7 @@ internal class MapboxWidgetRenderer(
   private val textures = intArrayOf(0)
   private val framebuffers = intArrayOf(0)
 
-  private val widgets = CopyOnWriteArrayList<Widget>()
+  private val widgets = CopyOnWriteArraySet<Widget>()
 
   private var width = 0
   private var height = 0
