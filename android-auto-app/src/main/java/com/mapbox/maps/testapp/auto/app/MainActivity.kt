2220e35d817837a16c36a976530b57d6d7fd923f
Kyle Madsen <kyle.mdsn@gmail.com>
2022-08-17 08:21:12 -0700

    Add activity to android auto app (#1606)

---
+++ android-auto-app/src/main/java/com/mapbox/maps/testapp/auto/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mapbox.maps.testapp.auto.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.maps.testapp.auto.R
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
