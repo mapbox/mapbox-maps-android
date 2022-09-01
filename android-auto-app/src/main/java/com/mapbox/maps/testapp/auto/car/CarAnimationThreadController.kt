758d39e700a229f2d35f5a3967290e71212827f8
Kyle Madsen <kyle.mdsn@gmail.com>
2022-09-01 11:31:38 -0700

    Add CarAnimationThreadController

---
+++ android-auto-app/src/main/java/com/mapbox/maps/testapp/auto/car/CarAnimationThreadController.kt
@@ -0,0 +1,23 @@
+package com.mapbox.maps.testapp.auto.car
+
+import com.mapbox.maps.MapboxExperimental
+import com.mapbox.maps.extension.androidauto.MapboxCarMapObserver
+import com.mapbox.maps.extension.androidauto.MapboxCarMapSurface
+import com.mapbox.maps.threading.AnimationThreadController
+
+/**
+ * This class is needed for Xiaomi devices. When the mobile app has been backgrounded and you are
+ * using android auto, Xiaomi MIUI will stop animations that are called from the main thread.
+ *
+ * https://github.com/mapbox/mapbox-maps-android/issues/1413
+ */
+@OptIn(MapboxExperimental::class)
+class CarAnimationThreadController : MapboxCarMapObserver {
+  override fun onAttached(mapboxCarMapSurface: MapboxCarMapSurface) {
+    AnimationThreadController.useBackgroundThread()
+  }
+
+  override fun onDetached(mapboxCarMapSurface: MapboxCarMapSurface) {
+    AnimationThreadController.useMainThread()
+  }
+}
