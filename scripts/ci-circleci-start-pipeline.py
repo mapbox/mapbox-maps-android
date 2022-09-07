8fd157a0ffb88c312ff6bbca9ad6914368b577c8
Alexander Kulikovskiy <aleksandr.kulikovskiy@mapbox.com>
2022-09-07 15:54:22 +0300

    port fix string flag (#1653)

--- scripts/ci-circleci-start-pipeline.py
+++ scripts/ci-circleci-start-pipeline.py
@@ -10,6 +10,16 @@ import datetime
 
 MAIN_BRANCH_NAME = "main"
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 
 def TriggerPipeline(slug, token, branch, params):
     url = "https://circleci.com/api/v2/project/github/%s/pipeline" % (slug)
