5dd509bfa04fb3afe185e919dc614f84781adbb8
Peng Liu <peng.liu@mapbox.com>
2022-09-20 11:57:16 +0300

    Remove mapboxAndroidTelemetry definition from project.

--- module-telemetry/build.gradle.kts
+++ module-telemetry/build.gradle.kts
@@ -26,7 +26,6 @@ android {
 dependencies {
   implementation(project(":sdk-base"))
   implementation(Dependencies.mapboxBase)
-//  implementation(Dependencies.mapboxAndroidTelemetry)
   compileOnly(Dependencies.mapboxAnnotations)
   kapt(Dependencies.mapboxAnnotationsProcessor)
   implementation(Dependencies.kotlin)
