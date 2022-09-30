46d94769cd4f0e6cc895baa2c5dc0c617b2f2c5d
Ramon Sadornil <ramon.sadornilrivera@mapbox.com>
2022-09-30 15:02:14 +0300

    Add Mapbox snapshot Maven repository configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,16 @@ allprojects {
       }
     }
     maven {
+      url = uri("https://api.mapbox.com/downloads/v2/snapshots/maven")
+      credentials {
+        username = "mapbox"
+        password = System.getenv("SDK_REGISTRY_TOKEN") ?: project.property("SDK_REGISTRY_TOKEN") as String
+      }
+      authentication {
+        create<BasicAuthentication>("basic")
+      }
+    }
+    maven {
       url = uri("https://oss.jfrog.org/artifactory/oss-snapshot-local/")
     }
   }
