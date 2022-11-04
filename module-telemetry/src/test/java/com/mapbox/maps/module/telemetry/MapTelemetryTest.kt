a34666c8352b68ec6c68ce8b38e11272809d588c
Peng Liu <peng.liu@mapbox.com>
2022-11-04 17:32:00 +0200

    Add unit tests.

--- module-telemetry/src/test/java/com/mapbox/maps/module/telemetry/MapTelemetryTest.kt
+++ module-telemetry/src/test/java/com/mapbox/maps/module/telemetry/MapTelemetryTest.kt
@@ -7,8 +7,7 @@ import android.view.WindowManager
 import com.mapbox.common.*
 import io.mockk.*
 import org.junit.After
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
@@ -121,6 +120,7 @@ class MapTelemetryTest {
     }
     val slot = slot<Event>()
     verify { eventsService.sendEvent(capture(slot), any()) }
+    assertEquals(EventPriority.QUEUED, slot.captured.priority)
     assertTrue(slot.captured.attributes.toJson().contains("\"event\":\"map.load\""))
     assertTrue(slot.captured.attributes.toJson().contains("\"sdkIdentifier\":\"mapbox-maps-android\""))
   }
@@ -170,6 +170,7 @@ class MapTelemetryTest {
     telemetry.onPerformanceEvent(null)
     val slot = slot<Event>()
     verify { eventsService.sendEvent(capture(slot), any()) }
+    assertEquals(EventPriority.QUEUED, slot.captured.priority)
     assertTrue(slot.captured.attributes.toJson().contains("\"event\":\"mobile.performance_trace\""))
   }
 }
