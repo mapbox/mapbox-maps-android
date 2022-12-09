e659e8458d54758d0a85b7c58876bcbe5dbe0fee
Ramon Sadornil <ramon.sadornilrivera@mapbox.com>
2022-12-09 10:52:19 +0200

    Handle displays changes and added comments

--- app/src/main/java/com/mapbox/maps/testapp/examples/SecondaryDisplayPresentationActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/SecondaryDisplayPresentationActivity.kt
@@ -26,6 +26,7 @@ private const val TAG = "SecondaryDisplayPresent"
 private const val DISPLAY_INFO_TAG = "DisplayInfo"
 
 class SecondaryDisplayPresentationActivity : AppCompatActivity() {
+  private lateinit var displayManager: DisplayManager
   private var currentPresentation: Presentation? = null
   private lateinit var binding: ActivitySecondaryDisplayPresentationBinding
   private val onDismissListener = DialogInterface.OnDismissListener { dialog ->
@@ -35,17 +36,57 @@ class SecondaryDisplayPresentationActivity : AppCompatActivity() {
       updateDisplaysInfo()
     }
   }
+  private val displayListener = object : DisplayManager.DisplayListener {
+    override fun onDisplayAdded(displayId: Int) {
+      Log.d(TAG, "Display #$displayId added.")
+      updateDisplaysInfo()
+    }
+
+    override fun onDisplayRemoved(displayId: Int) {
+      Log.d(TAG, "Display #$displayId removed.")
+      updateDisplaysInfo()
+    }
+
+    override fun onDisplayChanged(displayId: Int) {
+      Log.d(TAG, "Display #$displayId changed.")
+      updateDisplaysInfo()
+    }
+
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivitySecondaryDisplayPresentationBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    displayManager = getSystemService(DisplayManager::class.java)
+  }
 
   override fun onResume() {
     super.onResume()
-    binding = ActivitySecondaryDisplayPresentationBinding.inflate(layoutInflater)
-    setContentView(binding.root)
     updateDisplaysInfo()
+    // Register to receive events from the display manager.
+    displayManager.registerDisplayListener(displayListener, null)
+  }
+
+  override fun onPause() {
+    // Unregister from the display manager.
+    displayManager.unregisterDisplayListener(displayListener)
+    super.onPause()
+  }
+
+  override fun onStop() {
+    super.onStop()
+    // Dismiss the presentation when the activity is not visible.
+    currentPresentation?.let {
+      Log.i(TAG, "Dismissing presentation because the activity is no longer visible.");
+      it.dismiss();
+      currentPresentation = null;
+    }
   }
 
   private fun updateDisplaysInfo() {
     clearDisplaysInfo()
-    val displayManager = getSystemService(DisplayManager::class.java)
+
     val thisActivityDisplay = windowManager.defaultDisplay
     val displays = displayManager.displays
     val secondaryDisplays = displays.filterNot { it.displayId == thisActivityDisplay.displayId }
@@ -65,8 +106,8 @@ class SecondaryDisplayPresentationActivity : AppCompatActivity() {
         displayInfoBinding.displayBt.isEnabled = true
         displayInfoBinding.displayBt.setOnClickListener {
           updatePresentation(display)
-          updateDisplaysInfo()
         }
+        // Disable the PRESENT button for those displays that are already presenting
         currentPresentation?.let {
           if (it.display == display) {
             displayInfoBinding.displayBt.isEnabled = false
@@ -77,6 +118,7 @@ class SecondaryDisplayPresentationActivity : AppCompatActivity() {
   }
 
   private fun clearDisplaysInfo() {
+    // Remove all the views related to display info
     binding.root.children.filter { it.tag == DISPLAY_INFO_TAG }
       .toList() // Accumulate all the views to remove
       .forEach {
@@ -89,8 +131,8 @@ class SecondaryDisplayPresentationActivity : AppCompatActivity() {
     currentPresentation?.let { presentation ->
       // Dismiss the current presentation if the display has changed.
       if (presentation.display !== display) {
-        currentPresentation = null
         Log.i(TAG, "Dismissing old presentation. A new display will be used.")
+        currentPresentation = null
         presentation.dismiss()
       }
     }
@@ -104,15 +146,15 @@ class SecondaryDisplayPresentationActivity : AppCompatActivity() {
         presentation.show()
         presentation
       } catch (ex: WindowManager.InvalidDisplayException) {
-        Log.w(TAG, "Couldn't show presentation!  Display was removed in " + "the meantime.", ex)
+        Log.w(TAG, "Couldn't show presentation!  Display was removed in the meantime.", ex)
         null
       }
     }
+    updateDisplaysInfo()
   }
 
   /**
    * The presentation to show on the secondary display.
-   *
    *
    * Note that this display may have different metrics from the display on which
    * the main activity is showing so we must be careful to use the presentation's
@@ -123,11 +165,15 @@ class SecondaryDisplayPresentationActivity : AppCompatActivity() {
     Presentation(context, display), LifecycleOwner {
     private lateinit var lifecycleRegistry: LifecycleRegistry
     override fun onCreate(savedInstanceState: Bundle?) {
-      // Be sure to call the super class.
       super.onCreate(savedInstanceState)
 
+      // MapView uses Lifecycle events to handle its own state.
+      // Let's expose this Presentation lifecycle manually
       lifecycleRegistry = LifecycleRegistry(this)
       lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+
+      // Set the application theme which inherits from `Theme.AppCompat` which is needed for some
+      // of the views inside Mapview (e.g. CompassView)
       context.setTheme(R.style.AppTheme)
 
       // Notice that we are getting the context of the presentation.
@@ -139,13 +185,13 @@ class SecondaryDisplayPresentationActivity : AppCompatActivity() {
       super.onStart()
       // Only after we show does the dialog window actually return a decor view.
       val decorView: View = window!!.decorView
+      // Set the lifecycle owner for this dialog to be this class. This allows Mapview to get the
+      // right lifecycle registry
       ViewTreeLifecycleOwner.set(decorView, this)
       lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
-//      lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
     }
 
     override fun onStop() {
-//      lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
       lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
       lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
       super.onStop()
