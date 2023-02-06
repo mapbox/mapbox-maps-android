3a53946339900ea1064eecb56056bbc38f1fea87
ank27 <ankur.khandelwal@mapbox.com>
2023-02-06 16:56:59 +0200

    add example for tilecover

--- app/src/main/java/com/mapbox/maps/testapp/examples/SimpleMapActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/SimpleMapActivity.kt
@@ -1,20 +1,24 @@
 package com.mapbox.maps.testapp.examples
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Point
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxMap
+import com.mapbox.maps.TileCoverOptions
 
 /**
  * Example of displaying a map.
  */
 class SimpleMapActivity : AppCompatActivity() {
-
+  private lateinit var mapboxMap: MapboxMap
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     val mapView = MapView(this)
     setContentView(mapView)
+    mapboxMap = mapView.getMapboxMap()
     mapView.getMapboxMap()
       .apply {
         setCamera(
@@ -24,6 +28,12 @@ class SimpleMapActivity : AppCompatActivity() {
             .build()
         )
       }
+
+    val tileCoverOptions = TileCoverOptions.Builder().tileSize(256).build()
+    val tiles = mapboxMap.getTileCover(tileCoverOptions)
+    for (tile in tiles) {
+      Log.d("TILECOVER", "tile x = ${tile.x} y= ${tile.y}, z= ${tile.z}, tile = $tile")
+    }
   }
 
   companion object {
