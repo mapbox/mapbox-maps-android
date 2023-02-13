aa11fc5041b954fc217b08b7bd33bcbe9fd3cc56
Ramon Sadornil <ramon.sadornilrivera@mapbox.com>
2023-02-13 14:41:00 +0200

    Add flavor support to extension-compose

--- extension-compose/build.gradle.kts
+++ extension-compose/build.gradle.kts
@@ -21,6 +21,17 @@ android {
     }
   }
 
+  flavorDimensions.add("version")
+  productFlavors {
+    val private by creating {
+      dimension = "version"
+    }
+    val public by creating {
+      dimension = "version"
+      isDefault = true
+    }
+  }
+
   composeOptions {
     kotlinCompilerExtensionVersion = Versions.compose
   }
@@ -53,9 +64,19 @@ dependencies {
   }
 }
 
-tasks.withType<DokkaTask>().configureEach {
-  dokkaSourceSets {
-    configureEach {
+// let's register different Dokka Javadoc tasks per flavor
+android.productFlavors.all {
+  val flavor = name
+  tasks.register("${flavor}ReleaseDokkaJavadoc", DokkaTask::class.java) {
+    // We want to generate Javadoc so we copy the `dokkaJavadoc` task plugins dependencies in order
+    // to generate documentation in Javadoc format
+    val dokkaJavadocTask = tasks.findByName("dokkaJavadoc") as DokkaTask
+    plugins.dependencies.addAll(dokkaJavadocTask.plugins.allDependencies)
+    // Make sure we disable all the source sets not related to this flavour (and only for release build)
+    dokkaSourceSets.configureEach {
+      if (name != "main" && name != flavor && name != "${flavor}Release") {
+        suppress.set(true)
+      }
       reportUndocumented.set(true)
       failOnWarning.set(true)
     }
@@ -67,7 +88,7 @@ project.apply {
   from("$rootDir/gradle/lint.gradle")
   from("$rootDir/gradle/jacoco.gradle")
   from("$rootDir/gradle/sdk-registry.gradle")
-  from("$rootDir/gradle/track-public-apis.gradle")
+//  from("$rootDir/gradle/track-public-apis.gradle")
   from("$rootDir/gradle/detekt.gradle")
   from("$rootDir/gradle/dependency-updates.gradle")
 }
