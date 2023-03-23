c797a07f9e00786c7061cceca1c6bcb65cdb3a6f
Ramon <ramon.sadornilrivera@mapbox.com>
2023-03-23 15:03:41 +0200

    Fixed RevAPI job (#1460)

--- scripts/java-api-check-all.sh
+++ scripts/java-api-check-all.sh
@@ -13,24 +13,25 @@ then
 fi
 gh auth login --with-token < ./gh_token.txt
 
+REVAPI_VERSION="0.9.7"
 CURRENT_DIR=$(dirname "$0")
 MAJOR_CHANGE_FILE=${CURRENT_DIR}/../api_compat_report/major.txt
 [ -e $MAJOR_CHANGE_FILE ] && rm $MAJOR_CHANGE_FILE
 REVAPI=`mktemp -d`
 mkdir -p "${REVAPI}"
+REVAPI_PATH="$REVAPI/revapi-${REVAPI_VERSION}"
 
-if [[ -f "$REVAPI/revapi-0.9.7/revapi.sh" ]]; then
-  echo "Revapi has installed in $REVAPI/revapi-0.9.7/revapi.sh"
-  export PATH=${REVAPI}/revapi-0.9.7:$PATH
+if [[ -f "$REVAPI_PATH/revapi.sh" ]]; then
+  echo "RevAPI already installed in $REVAPI_PATH"
 fi
 if ! [ -x "$(command -v revapi.sh)" ]; then
-    echo "Revapi is not installed. Installing revapi"
+    echo "RevAPI is not installed. Installing RevAPI ${REVAPI_VERSION}"
     pushd "${REVAPI}" > /dev/null
-    wget https://search.maven.org/remotecontent?filepath=org/revapi/revapi-standalone/0.9.7/revapi-standalone-0.9.7-standalone.zip -O revapi.zip \
-    && unzip revapi.zip \
-    && export PATH=${REVAPI}/revapi-0.9.7:$PATH
+    wget https://search.maven.org/remotecontent?filepath=org/revapi/revapi-standalone/${REVAPI_VERSION}/revapi-standalone-${REVAPI_VERSION}-standalone.zip -O revapi.zip \
+    && unzip revapi.zip
     popd > /dev/null
 fi
+export PATH=$REVAPI_PATH:$PATH
 
 RELEASE_TAG=${1-""}
 
@@ -38,11 +39,25 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 TARGET_BRANCH=$(gh pr view "$CURRENT_BRANCH" --json baseRefName --jq '.baseRefName')
 echo "CURRENT_BRANCH: $CURRENT_BRANCH"
 echo "TARGET_BRANCH: $TARGET_BRANCH"
-readonly STABLE_RELEASE_TAG_PATTERN="v10\.[0-9]*\.0$"
+VARIANTS=( private public )
 
-if [[ "$TARGET_BRANCH" = "main"] || "$TARGET_BRANCH" = "develop" ]]; then
+# If "main" then we're targeting v10 releases
+if [[ "$TARGET_BRANCH" = "main" ]]; then
+  STABLE_RELEASE_TAG_PATTERN="v10\.[0-9]*\.0$"
   # use the latest stable minor release tag for main branch
-  LAST_STABLE_RELEASE_TAG=$(git tag --list --sort=-creatordate | grep $STABLE_RELEASE_TAG_PATTERN | head -n 1)
+  LAST_STABLE_RELEASE_TAG=$(git tag --list --sort=-creatordate | grep "$STABLE_RELEASE_TAG_PATTERN" | head -n 1)
+# If "develop" then we're targeting v11 releases
+elif [[ "$TARGET_BRANCH" = "develop" ]]; then
+  STABLE_RELEASE_TAG_PATTERN="v11\.[0-9]*\.0$"
+  # use the latest stable minor release tag for develop branch if available or empty if not
+  LAST_STABLE_RELEASE_TAG=$(git tag --list --sort=-creatordate | grep "$STABLE_RELEASE_TAG_PATTERN" | head -n 1 || true)
+  if [ -z "$LAST_STABLE_RELEASE_TAG" ]; then
+    VARIANTS=( public )
+    echo "========================  No v11 stable release for $TARGET_BRANCH found. Comparing against most recent one. ===================================="
+    STABLE_RELEASE_TAG_PATTERN="v11\.[0-9]*\.[0-9]*.*$"
+    # use the latest release tag for develop branch if available (e.g. [v11.0.0-alpha.1])
+    LAST_STABLE_RELEASE_TAG=$(git tag --list --sort=-creatordate | grep "$STABLE_RELEASE_TAG_PATTERN" | head -n 1)
+  fi
 else
   LATEST_MINOR_THIS_BRANCH=$(git tag --sort=-creatordate | { grep "^$TARGET_BRANCH\.0$" || test $? = 1; } | head -n 1) # grep sets error if result is empty, so `test` is needed
   if [ -z "$LATEST_MINOR_THIS_BRANCH" ]; then
@@ -61,20 +76,27 @@ else
 fi
 echo "LAST_STABLE_RELEASE_TAG: $LAST_STABLE_RELEASE_TAG"
 echo "Release tag: $RELEASE_TAG"
+echo "Variants: $VARIANTS"
 MODULES=$(sed -e '/include(/,/)/!d' ./settings.gradle.kts | tr -d '\n' | sed 's/.*(\([^]]*\)).*/\1/g;s/,\n/\n/g' | tr , \\n | sed 's/.*":\([^]]*\)".*/\1/g')
 for i in $MODULES; do
-  if [ -f "./$i/build/outputs/aar/$i-release.aar" ]; then
-    MODULE_NAME=$i
-    if [[ $i == plugin* ]]; then
-      MODULE_NAME=${MODULE_NAME: 7}
-    elif [[ $i == extension* ]]; then
-      MODULE_NAME=${MODULE_NAME: 10}
-    elif [[ $i == *telemetry* ]]; then
-      continue
+  for variant in "${VARIANTS[@]}"; do
+    echo "========================  Checking module: $i (variant $variant) ===================================="
+    AAR_PATH="./$i/build/outputs/aar/$i-$variant-release.aar"
+    if [ -f $AAR_PATH ]; then
+      MODULE_NAME=$i
+      if [[ $i == plugin* ]]; then
+        MODULE_NAME=${MODULE_NAME: 7}
+      elif [[ $i == extension* ]]; then
+        MODULE_NAME=${MODULE_NAME: 10}
+      elif [[ $i == *telemetry* ]]; then
+        continue
+      fi
+      echo "========================  module aar name: $MODULE_NAME  ===================================="
+      "${CURRENT_DIR}"/java-api-check.sh "$RELEASE_TAG" "$AAR_PATH" "$MODULE_NAME" "$LAST_STABLE_RELEASE_TAG"
+    else
+      echo "======================== WARNING: no release aar file found: $AAR_PATH ===================================="
     fi
-    echo "========================  Checking module: $i, module aar name: $MODULE_NAME  ===================================="
-    "${CURRENT_DIR}"/java-api-check.sh "$RELEASE_TAG" "./$i/build/outputs/aar/$i-release.aar" "$MODULE_NAME" "$LAST_STABLE_RELEASE_TAG"
-  fi
+  done
 done
 
 rm -rf "${REVAPI}"
