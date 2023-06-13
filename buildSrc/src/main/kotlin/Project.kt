c5323a5aa161616608dcdedc64a8ad43bf0dbab6
Alexander Kulikovskiy <aleksandr.kulikovskiy@mapbox.com>
2023-06-13 16:26:30 +0300

    bump gl-native to 10.14.0, common to 23.6.0 (#1788)
    * bump gl-native to 10.14.0, common to 23.6.0
    
    * publish_testapp_playstore
    
    * fix mapbox_hash parameter

--- buildSrc/src/main/kotlin/Project.kt
+++ buildSrc/src/main/kotlin/Project.kt
@@ -113,8 +113,8 @@ object Versions {
   const val mapboxGestures = "0.8.0"
   const val mapboxJavaServices = "5.4.1"
   const val mapboxBase = "0.8.0"
-  const val mapboxGlNative = "10.14.0-rc.1"
-  const val mapboxCommon = "23.6.0-rc.1"
+  const val mapboxGlNative = "10.14.0"
+  const val mapboxCommon = "23.6.0"
   const val androidxCore = "1.7.0" // last version compatible with kotlin 1.5.31
   const val androidxFragmentTesting = "1.5.0"
   const val androidxAnnotation = "1.1.0"
