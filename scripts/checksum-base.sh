646e0de1d3d6088226e1d9d34e2f4ec69e4b879a
Alexander Kulikovskiy <aleksandr.kulikovskiy@mapbox.com>
2023-07-27 15:08:52 +0300

    add --no-performance-metrics for firebase runs (#1870)
    * add --no-performance-metrics for firebase runs
    
    * add missed gradle cache
    
    * add checksum script for top level project
    
    * review fixes

---
+++ scripts/checksum-base.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+RESULT_FILE=$1
+FILES=()
+
+if [ -f $RESULT_FILE ]; then
+  rm $RESULT_FILE
+fi
+
+FILE="`basename "${RESULT_FILE}"`"
+DIR="`dirname "${RESULT_FILE}"`"
+mkdir -p "${DIR}" && touch "${DIR}/${FILE}"
+
+add_files() {
+    find $1 -type f -name $2  -print0 >tmpfile
+    while IFS=  read -r -d $'\0'; do
+        FILES+=("$REPLY")
+    done <tmpfile
+    rm -f tmpfile
+}
+
+checksum_file() {
+    echo `openssl md5 $1 | awk '{print $2}'`
+}
+
+generate_checksums() {
+    for FILE in ${FILES[@]}; do
+    	echo `checksum_file $FILE` >> $RESULT_FILE
+    done
+
+    sort $RESULT_FILE -o $RESULT_FILE
+
+    echo "New hash sum file:"
+    cat $RESULT_FILE
+}
--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -1,36 +1,8 @@
-#!/bin/bash
-RESULT_FILE=$1
-FILES=()
-
-if [ -f $RESULT_FILE ]; then
-  rm $RESULT_FILE
-fi
-
-FILE="`basename "${RESULT_FILE}"`"
-DIR="`dirname "${RESULT_FILE}"`"
-mkdir -p "${DIR}" && touch "${DIR}/${FILE}"
-
-add_files() {
-    find $1 -type f -name $2  -print0 >tmpfile
-    while IFS=  read -r -d $'\0'; do
-        FILES+=("$REPLY")
-    done <tmpfile
-    rm -f tmpfile
-}
-
-checksum_file() {
-    echo `openssl md5 $1 | awk '{print $2}'`
-}
+#!/usr/bin/env bash
+source scripts/checksum-base.sh
 
 add_files . build.gradle.kts
 add_files ./gradle/libs.versions.toml
 add_files ./gradle/wrapper gradle-wrapper.properties
 
-for FILE in ${FILES[@]}; do
-	echo `checksum_file $FILE` >> $RESULT_FILE
-done
-
-sort $RESULT_FILE -o $RESULT_FILE
-
-echo "New hash sum file:"
-cat $RESULT_FILE
+generate_checksums
