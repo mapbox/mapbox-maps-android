51c38ea83224fb330209fc3777e2358f2599017c
Kiryl Dzehtsiarenka <kiryl.dzehtsiarenka@mapbox.com>
2023-10-26 10:50:35 +0300

    Fix a leak in MapboxMapRecorderActivity (#2097)
    * Fix a leak in MapboxMapRecorderActivity
    
    * Always stop recorder
    
    * Replace toast with log

--- app/src/main/java/com/mapbox/maps/testapp/examples/MapboxMapRecorderActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/MapboxMapRecorderActivity.kt
@@ -3,7 +3,6 @@ package com.mapbox.maps.testapp.examples
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
@@ -11,9 +10,11 @@ import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.MapboxMapRecorder
 import com.mapbox.maps.Style
 import com.mapbox.maps.dsl.cameraOptions
+import com.mapbox.maps.logI
 import com.mapbox.maps.mapPlayerOptions
 import com.mapbox.maps.plugin.animation.MapAnimationOptions.Companion.mapAnimationOptions
 import com.mapbox.maps.plugin.animation.flyTo
+import com.mapbox.maps.plugin.gestures.gestures
 
 /**
  * Showcase how to use [MapboxMapRecorder] to record and replay sessions.
@@ -21,11 +22,15 @@ import com.mapbox.maps.plugin.animation.flyTo
 @OptIn(MapboxExperimental::class)
 class MapboxMapRecorderActivity : AppCompatActivity() {
 
+  private var activityStopped = false
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     val mapView = MapView(this)
     setContentView(mapView)
 
+    // do not allow panning the map in this activity
+    mapView.gestures.scrollEnabled = false
     val mapboxMap = mapView.mapboxMap
     // Make the ``MapboxMapRecorder`` and start the recording
     val recorder = mapboxMap.createRecorder()
@@ -43,9 +48,12 @@ class MapboxMapRecorderActivity : AppCompatActivity() {
         object : AnimatorListenerAdapter() {
           override fun onAnimationEnd(animation: Animator) {
             // When the camera animation is complete, stop the map recording
-            // Replay the camera animation twice at double speed by passing the recorded sequence returned from the stop method
             val recording = recorder.stopRecording()
-            Toast.makeText(this@MapboxMapRecorderActivity, "Replay started", Toast.LENGTH_SHORT).show()
+            if (activityStopped) {
+              return
+            }
+            // Replay the camera animation twice at double speed by passing the recorded sequence returned from the stop method
+            logI(TAG, "Replay started")
             recorder.replay(
               recording,
               mapPlayerOptions {
@@ -54,11 +62,21 @@ class MapboxMapRecorderActivity : AppCompatActivity() {
                 avoidPlaybackPauses(false)
               }
             ) {
-              Toast.makeText(this@MapboxMapRecorderActivity, "Replay ended, state = ${recorder.getPlaybackState()}", Toast.LENGTH_SHORT).show()
+              logI(TAG, "Replay ended, state = ${recorder.getPlaybackState()}")
             }
           }
         }
       )
     }
+  }
+
+  override fun onStop() {
+    activityStopped = true
+    // super.onStop will cancel the animation above and call onAnimationEnd
+    super.onStop()
+  }
+
+  private companion object {
+    private const val TAG = "MapboxMapRecorderActivity"
   }
 }
