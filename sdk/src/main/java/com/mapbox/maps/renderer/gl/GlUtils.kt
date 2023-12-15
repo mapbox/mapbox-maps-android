046d453dea744ed59fee681d322026cc7025059e
Ramon <ramon.sadornilrivera@mapbox.com>
2023-12-15 17:26:37 +0200

    Inline GlUtils.checkError to avoid extra calls in release (#2185)

--- sdk/src/main/java/com/mapbox/maps/renderer/gl/GlUtils.kt
+++ sdk/src/main/java/com/mapbox/maps/renderer/gl/GlUtils.kt
@@ -39,21 +39,29 @@ internal object GlUtils {
       GLES20.glCompileShader(shader)
     }
 
-  fun checkError(cmd: String? = null) {
+  // We inline this function so in release flavour it is completely removed and no call is done
+  @Suppress("NOTHING_TO_INLINE")
+  inline fun checkError(cmd: String? = null) {
     if (BuildConfig.DEBUG) {
-      when (val error = GLES20.glGetError()) {
-        GLES20.GL_NO_ERROR -> {}
-        else -> throw java.lang.RuntimeException(
-          "$cmd - error in GL : ${when (error) {
+      throwIfError(cmd)
+    }
+  }
+
+  private fun throwIfError(cmd: String?) {
+    when (val error = GLES20.glGetError()) {
+      GLES20.GL_NO_ERROR -> {}
+      else -> throw java.lang.RuntimeException(
+        "$cmd - error in GL : ${
+          when (error) {
             GLES20.GL_INVALID_ENUM -> "GL_INVALID_ENUM"
             GLES20.GL_INVALID_VALUE -> "GL_INVALID_VALUE"
             GLES20.GL_INVALID_OPERATION -> "GL_INVALID_OPERATION"
             GLES20.GL_INVALID_FRAMEBUFFER_OPERATION -> "GL_INVALID_FRAMEBUFFER_OPERATION"
             GLES20.GL_OUT_OF_MEMORY -> "GL_OUT_OF_MEMORY"
             else -> error
-          }}"
-        )
-      }
+          }
+        }"
+      )
     }
   }
 
