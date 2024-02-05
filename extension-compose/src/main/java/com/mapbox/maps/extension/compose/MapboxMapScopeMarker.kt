9ceb610ef881772d26113b4e75b0ef21a0b246b2
Peng Liu <peng.liu@mapbox.com>
2024-02-05 14:46:27 +0200

    [compose] Fix warnings that using UI composable where Mapbox Map composable is expected. (#2249)

---
+++ extension-compose/src/main/java/com/mapbox/maps/extension/compose/MapboxMapScopeMarker.kt
@@ -0,0 +1,7 @@
+package com.mapbox.maps.extension.compose
+
+/**
+ * Marks the scope of DSL used within [MapboxMap] composable function.
+ */
+@DslMarker
+public annotation class MapboxMapScopeMarker
