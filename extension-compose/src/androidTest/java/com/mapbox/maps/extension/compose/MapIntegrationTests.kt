de47a6e54cf81e75a50ec29dcd03111f64efe0fd
Natig Babayev <natig.babayev@mapbox.com>
2024-07-01 13:13:23 +0300

    [compose] Add UI tests for annotations and map (#2548)
    * [compose] Add UI tests for annotations and map
    
    * Update tests according to latest Mapbox compose extension changes
    
    * Remove unnecessary CountDownLatch
    
    * Remove countdown latch dependency and make sure test waits until box is drawn over annotation
    
    * Increase waiting time to make sure style is loaded
    
    * Use style that loads faster for test
    
    * Address review comments
    
    * Lint fix

---
+++ extension-compose/src/androidTest/java/com/mapbox/maps/extension/compose/MapIntegrationTests.kt
@@ -0,0 +1,417 @@
+package com.mapbox.maps.extension.compose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.doubleClick
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.test.swipeLeft
+import androidx.compose.ui.test.swipeUp
+import androidx.test.platform.app.InstrumentationRegistry
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraState
+import com.mapbox.maps.MapView
+import com.mapbox.maps.MapboxExperimental
+import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.extension.compose.internal.utils.CityLocations
+import com.mapbox.maps.plugin.gestures.generated.GesturesSettings
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(MapboxExperimental::class)
+public class MapIntegrationTests {
+
+  @get:Rule
+  public val composeTestRule: ComposeContentTestRule = createComposeRule()
+
+  @Test
+  public fun testMapViewWhenMapIsLoaded() {
+    var mapView: MapView? = null
+    composeTestRule.setContent {
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapState = rememberMapState()
+        ) {
+          MapEffect(Unit) {
+            mapView = it
+          }
+        }
+      }
+    }
+    composeTestRule.waitUntil(timeoutMillis = 3_000) { mapView != null }
+  }
+
+  @Test
+  public fun testMapDoubleTap() {
+    lateinit var cameraState: CameraState
+    composeTestRule.setContent {
+      val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+          zoom(ZOOM)
+          center(CityLocations.HELSINKI)
+        }
+      }
+      val mapState = rememberMapState()
+      LaunchedEffect(Unit) {
+        mapState.cameraChangedEvents.collect {
+          cameraState = it.cameraState
+        }
+      }
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = mapViewportState,
+          mapState = mapState
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(MAP_TEST_TAG).performTouchInput { doubleClick() }
+    composeTestRule.waitUntil {
+      cameraState.zoom == ZOOM + 1
+    }
+  }
+
+  @Test
+  public fun testMapDoubleTapDisabled() {
+    lateinit var cameraState: CameraState
+    composeTestRule.setContent {
+      val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+          zoom(ZOOM)
+          center(CityLocations.HELSINKI)
+        }
+      }
+      val mapState = rememberMapState {
+        gesturesSettings = GesturesSettings { doubleTapToZoomInEnabled = false }
+      }
+      LaunchedEffect(Unit) {
+        mapState.cameraChangedEvents.collect {
+          cameraState = it.cameraState
+        }
+      }
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = mapViewportState,
+          mapState = mapState,
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(MAP_TEST_TAG).performTouchInput { doubleClick() }
+    assertEquals(ZOOM, cameraState.zoom, EPS)
+  }
+
+  @Test
+  public fun testMapPinchToZoomOut() {
+    lateinit var cameraState: CameraState
+    composeTestRule.setContent {
+      val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+          zoom(ZOOM)
+          center(CityLocations.HELSINKI)
+        }
+      }
+      val mapState = rememberMapState()
+      LaunchedEffect(Unit) {
+        mapState.cameraChangedEvents.collect {
+          cameraState = it.cameraState
+        }
+      }
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = mapViewportState,
+          mapState = mapState,
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(MAP_TEST_TAG).performTouchInput {
+      down(0, center + Offset(0f, -10f))
+      down(1, center + Offset(0f, 10f))
+
+      moveBy(0, Offset(0f, -50f))
+      moveBy(1, Offset(0f, +50f))
+      up(0)
+      up(1)
+    }
+    composeTestRule.waitUntil {
+      cameraState.zoom < ZOOM
+    }
+  }
+
+  @Test
+  public fun testMapScrollDisabled() {
+    lateinit var cameraState: CameraState
+    composeTestRule.setContent {
+      val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+          zoom(ZOOM)
+          center(CityLocations.HELSINKI)
+        }
+      }
+      val mapState = rememberMapState {
+        gesturesSettings = GesturesSettings {
+          scrollEnabled = false
+        }
+      }
+
+      LaunchedEffect(Unit) {
+        mapState.cameraChangedEvents.collect {
+          cameraState = it.cameraState
+        }
+      }
+
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapState = mapState,
+          mapViewportState = mapViewportState,
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(MAP_TEST_TAG).performTouchInput {
+      swipeLeft()
+      swipeUp()
+    }
+    assertEquals(CityLocations.HELSINKI.latitude(), cameraState.center.latitude(), EPS)
+    assertEquals(CityLocations.HELSINKI.longitude(), cameraState.center.longitude(), EPS)
+  }
+
+  @Test
+  public fun testMapScrollHorizontally() {
+    lateinit var cameraState: CameraState
+    composeTestRule.setContent {
+      val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+          zoom(ZOOM)
+          center(CityLocations.HELSINKI)
+        }
+      }
+      val mapState = rememberMapState()
+      LaunchedEffect(Unit) {
+        mapState.cameraChangedEvents.collect {
+          cameraState = it.cameraState
+        }
+      }
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = mapViewportState,
+          mapState = mapState,
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(MAP_TEST_TAG).performTouchInput {
+      swipeLeft()
+      swipeUp()
+    }
+    composeTestRule.waitUntil {
+      cameraState.center.longitude() != CityLocations.HELSINKI.longitude() &&
+        cameraState.center.latitude() != CityLocations.HELSINKI.latitude()
+    }
+  }
+
+  @Test
+  public fun testMapPinchToZoomDisabled() {
+    lateinit var cameraState: CameraState
+    composeTestRule.setContent {
+      val mapViewportState = rememberMapViewportState {
+        setCameraOptions {
+          zoom(ZOOM)
+          center(CityLocations.HELSINKI)
+        }
+      }
+      val mapState = rememberMapState {
+        gesturesSettings = GesturesSettings { pinchToZoomEnabled = false }
+      }
+      LaunchedEffect(Unit) {
+        mapState.cameraChangedEvents.collect {
+          cameraState = it.cameraState
+        }
+      }
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = mapViewportState,
+          mapState = mapState,
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(MAP_TEST_TAG).performTouchInput {
+      down(0, center + Offset(0f, -10f))
+      down(1, center + Offset(0f, 10f))
+
+      moveBy(0, Offset(0f, -50f))
+      moveBy(1, Offset(0f, +50f))
+      up(0)
+      up(1)
+    }
+    composeTestRule.waitUntil { ZOOM == cameraState.zoom }
+  }
+
+  @Test
+  public fun testMapOrnaments() {
+    val compassTag = "compass"
+    val scaleBarTag = "scalebar"
+    val logoTag = "logo"
+    val attributionTag = "attribution"
+    composeTestRule.setContent {
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = rememberMapViewportState {
+            setCameraOptions {
+              zoom(ZOOM)
+              center(CityLocations.HELSINKI)
+              bearing(30.0)
+            }
+          },
+          compass = {
+            Compass(modifier = Modifier.testTag(compassTag))
+          },
+          scaleBar = {
+            ScaleBar(modifier = Modifier.testTag(scaleBarTag))
+          },
+          logo = {
+            Logo(modifier = Modifier.testTag(logoTag))
+          },
+          attribution = {
+            Attribution(modifier = Modifier.testTag(attributionTag))
+          }
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(compassTag).assertExists()
+    composeTestRule.onNodeWithTag(scaleBarTag).assertExists()
+    composeTestRule.onNodeWithTag(logoTag).assertExists()
+    composeTestRule.onNodeWithTag(attributionTag).assertExists().performClick()
+    val attributionDialogTitle = InstrumentationRegistry
+      .getInstrumentation()
+      .context
+      .resources
+      .getString(R.string.mapbox_attributionsDialogTitle)
+    composeTestRule.onNodeWithText(attributionDialogTitle).assertExists()
+  }
+
+  @Test
+  public fun testMapWithCompass() {
+    val compassTag = "compass"
+    var bearing = -1.0
+    composeTestRule.setContent {
+      val mapState = rememberMapState()
+      LaunchedEffect(Unit) {
+        mapState.cameraChangedEvents.collect {
+          bearing = it.cameraState.bearing
+        }
+      }
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = rememberMapViewportState {
+            setCameraOptions {
+              zoom(ZOOM)
+              center(CityLocations.HELSINKI)
+              bearing(30.0)
+            }
+          },
+          mapState = mapState,
+          compass = {
+            Compass(modifier = Modifier.testTag(compassTag))
+          }
+        )
+      }
+    }
+    composeTestRule.onNodeWithTag(compassTag)
+      .assertExists()
+      .performClick()
+    composeTestRule.onNodeWithContentDescription(compassTag).assertDoesNotExist()
+    composeTestRule.waitUntil { bearing == 0.0 }
+  }
+
+  @Test
+  public fun testMapEffectWithState() {
+    var actualRefreshCount = 0
+    val contentDescription = "Increment"
+    composeTestRule.setContent {
+      var count by remember {
+        mutableStateOf(0)
+      }
+      Scaffold(
+        floatingActionButton = {
+          FloatingActionButton(onClick = { count += 1 }) {
+            Icon(Icons.Filled.Add, contentDescription)
+          }
+        }
+      ) { padding ->
+        MapboxMap(
+          modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .testTag(MAP_TEST_TAG)
+        ) {
+          MapEffect(count) {
+            actualRefreshCount += 1
+          }
+        }
+      }
+    }
+    composeTestRule.onNodeWithContentDescription(contentDescription)
+      .performClick()
+      .performClick()
+    composeTestRule.waitUntil { actualRefreshCount == 2 }
+  }
+
+  @Test
+  public fun testMapViewPort() {
+    var actualZoom = 0.0
+    lateinit var actualCenter: Point
+    composeTestRule.setContent {
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = rememberMapViewportState {
+            setCameraOptions {
+              zoom(ZOOM)
+              center(CityLocations.HELSINKI)
+            }
+          },
+        ) {
+          MapEffect(Unit) {
+            actualZoom = it.mapboxMap.cameraState.zoom
+            actualCenter = it.mapboxMap.cameraState.center
+          }
+        }
+      }
+    }
+    assertEquals(ZOOM, actualZoom, EPS)
+    assertEquals(CityLocations.HELSINKI.latitude(), actualCenter.latitude(), EPS)
+    assertEquals(CityLocations.HELSINKI.longitude(), actualCenter.longitude(), EPS)
+  }
+
+  private companion object {
+    private const val MAP_TEST_TAG = "map_tag"
+    private const val ZOOM: Double = 10.0
+    private const val EPS = 0.0001
+  }
+}
---
+++ extension-compose/src/androidTest/java/com/mapbox/maps/extension/compose/annotation/AnnotationTests.kt
@@ -0,0 +1,393 @@
+package com.mapbox.maps.extension.compose.annotation
+
+import android.graphics.Color
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapboxExperimental
+import com.mapbox.maps.Style
+import com.mapbox.maps.coroutine.awaitStyle
+import com.mapbox.maps.extension.compose.MapEffect
+import com.mapbox.maps.extension.compose.MapboxMap
+import com.mapbox.maps.extension.compose.MapboxMapComposable
+import com.mapbox.maps.extension.compose.R
+import com.mapbox.maps.extension.compose.animation.viewport.MapViewportState
+import com.mapbox.maps.extension.compose.annotation.generated.CircleAnnotation
+import com.mapbox.maps.extension.compose.annotation.generated.CircleAnnotationGroup
+import com.mapbox.maps.extension.compose.annotation.generated.PointAnnotation
+import com.mapbox.maps.extension.compose.annotation.generated.PointAnnotationGroup
+import com.mapbox.maps.extension.compose.annotation.generated.PolygonAnnotation
+import com.mapbox.maps.extension.compose.annotation.generated.PolylineAnnotation
+import com.mapbox.maps.extension.compose.annotation.generated.PolylineAnnotationGroup
+import com.mapbox.maps.extension.compose.internal.utils.CityLocations.HELSINKI
+import com.mapbox.maps.extension.compose.style.MapStyle
+import com.mapbox.maps.plugin.annotation.AnnotationConfig
+import com.mapbox.maps.plugin.annotation.AnnotationSourceOptions
+import com.mapbox.maps.plugin.annotation.ClusterOptions
+import com.mapbox.maps.plugin.annotation.generated.CircleAnnotation
+import com.mapbox.maps.plugin.annotation.generated.CircleAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotation
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.PolygonAnnotation
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotation
+import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotationOptions
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(MapboxExperimental::class)
+public class AnnotationTests {
+
+  @get:Rule
+  public val composeTestRule: ComposeContentTestRule = createComposeRule()
+
+  @Test
+  public fun testCircleAnnotation() {
+    var clickedAnnotation: CircleAnnotation? = null
+    val testTag = "circle_annotation"
+
+    setMapContent(
+      cameraCenter = HELSINKI,
+      annotationPoint = SINGLE_POINT,
+      testTag
+    ) {
+      CircleAnnotation(
+        point = SINGLE_POINT,
+        circleRadius = 20.0,
+        circleColorInt = Color.BLUE,
+        onClick = {
+          clickedAnnotation = it
+          true
+        }
+      )
+    }
+
+    composeTestRule.onNodeWithTag(testTag).performClick()
+    composeTestRule.waitUntil { clickedAnnotation != null }
+    assertEquals(SINGLE_POINT, clickedAnnotation!!.point)
+  }
+
+  @Test
+  public fun testCircleAnnotationGroup() {
+    var clickedAnnotation: CircleAnnotation? = null
+    val testTag = "circle_annotation"
+    setMapContent(
+      cameraCenter = HELSINKI,
+      annotationPoint = CLUSTER_POINTS[2],
+      zoom = ZOOM + 2,
+      testTag = testTag
+    ) {
+      CircleAnnotationGroup(
+        annotations = CLUSTER_POINTS.map {
+          CircleAnnotationOptions()
+            .withPoint(it)
+            .withCircleRadius(10.0)
+            .withCircleColor(Color.RED)
+        },
+        annotationConfig = AnnotationConfig(
+          annotationSourceOptions = AnnotationSourceOptions(
+            clusterOptions = ClusterOptions(
+              colorLevels = listOf(
+                Pair(100, Color.RED),
+                Pair(50, Color.BLUE),
+                Pair(0, Color.GREEN)
+              )
+            )
+          )
+        ),
+        onClick = {
+          clickedAnnotation = it
+          true
+        }
+      )
+    }
+    composeTestRule.onNodeWithTag(testTag).performClick()
+    composeTestRule.waitUntil { clickedAnnotation != null }
+    assertEquals(CLUSTER_POINTS[2], clickedAnnotation!!.point)
+    assertEquals(Color.RED, clickedAnnotation!!.circleColorInt)
+  }
+
+  @Test
+  public fun testPointAnnotation() {
+    var clickedAnnotation: PointAnnotation? = null
+    val testTag = "point_annotation"
+
+    setMapContent(
+      cameraCenter = HELSINKI,
+      annotationPoint = SINGLE_POINT,
+      testTag
+    ) {
+      PointAnnotation(
+        point = SINGLE_POINT,
+        iconImage = "bitmap",
+        onClick = {
+          clickedAnnotation = it
+          true
+        }
+      )
+
+      MapEffect(Unit) { mapView ->
+        mapView.mapboxMap.loadStyle(Style.STANDARD) {
+          it.addImage(
+            "bitmap",
+            ContextCompat.getDrawable(mapView.context, R.drawable.mapbox_mylocation_icon_bearing)!!
+              .toBitmap()
+          )
+        }
+      }
+    }
+
+    composeTestRule.onNodeWithTag(testTag).performClick()
+    composeTestRule.waitUntil { clickedAnnotation != null }
+    assertEquals(SINGLE_POINT, clickedAnnotation!!.point)
+  }
+
+  @Test
+  public fun testPointAnnotationGroup() {
+    var clickedAnnotation: PointAnnotation? = null
+    val testTag = "point_annotation"
+    setMapContent(
+      cameraCenter = HELSINKI,
+      annotationPoint = CLUSTER_POINTS[2],
+      zoom = ZOOM + 2,
+      testTag = testTag
+    ) {
+      PointAnnotationGroup(
+        annotations = CLUSTER_POINTS.map {
+          PointAnnotationOptions()
+            .withIconImage("bitmap")
+            .withPoint(it)
+        },
+        annotationConfig = AnnotationConfig(
+          annotationSourceOptions = AnnotationSourceOptions(
+            clusterOptions = ClusterOptions(
+              colorLevels = listOf(
+                Pair(100, Color.RED),
+                Pair(50, Color.BLUE),
+                Pair(0, Color.GREEN)
+              )
+            )
+          )
+        ),
+        onClick = {
+          clickedAnnotation = it
+          true
+        }
+      )
+
+      MapEffect(Unit) { mapView ->
+        mapView.mapboxMap.loadStyle(Style.STANDARD) {
+          it.addImage(
+            "bitmap",
+            ContextCompat.getDrawable(mapView.context, R.drawable.mapbox_mylocation_icon_bearing)!!
+              .toBitmap()
+          )
+        }
+      }
+    }
+
+    composeTestRule.onNodeWithTag(testTag).performClick()
+    composeTestRule.waitUntil { clickedAnnotation != null }
+    assertEquals(CLUSTER_POINTS[2], clickedAnnotation!!.point)
+  }
+
+  @Test
+  public fun testPolygonAnnotation() {
+    var clickedAnnotation: PolygonAnnotation? = null
+    val testTag = "polygon_annotation"
+
+    setMapContent(
+      cameraCenter = HELSINKI,
+      annotationPoint = POLYGON_POINTS.first().first(),
+      testTag = testTag
+    ) {
+      PolygonAnnotation(
+        points = POLYGON_POINTS,
+        fillColorInt = Color.RED,
+        onClick = {
+          clickedAnnotation = it
+          true
+        }
+      )
+    }
+    composeTestRule.onNodeWithTag(testTag).performClick()
+    composeTestRule.waitUntil { clickedAnnotation != null }
+    assertEquals(Color.RED, clickedAnnotation!!.fillColorInt)
+    assertEquals(POLYGON_POINTS, clickedAnnotation!!.points)
+  }
+
+  @Test
+  public fun testPolylineAnnotation() {
+    var clickedAnnotation: PolylineAnnotation? = null
+    val testTag = "polyline_annotation"
+    setMapContent(
+      cameraCenter = HELSINKI,
+      annotationPoint = HELSINKI,
+      testTag = testTag
+    ) {
+      PolylineAnnotation(
+        points = POLYLINE_POINTS,
+        lineColorInt = Color.RED,
+        lineWidth = 15.0,
+        onClick = {
+          clickedAnnotation = it
+          true
+        }
+      )
+    }
+    composeTestRule.onNodeWithTag(testTag).performClick()
+    composeTestRule.waitUntil { clickedAnnotation != null }
+    assertEquals(Color.RED, clickedAnnotation!!.lineColorInt)
+    assertEquals(POLYLINE_POINTS, clickedAnnotation!!.points)
+  }
+
+  @Test
+  public fun testPolylineAnnotationGroup() {
+    var clickedAnnotation: PolylineAnnotation? = null
+    val testTag = "polyline_annotation"
+    setMapContent(
+      cameraCenter = HELSINKI,
+      annotationPoint = HELSINKI,
+      testTag = testTag
+    ) {
+      PolylineAnnotationGroup(
+        annotations = POLYLINE_GROUP_POINTS.map {
+          PolylineAnnotationOptions()
+            .withPoints(it)
+            .withLineWidth(15.0)
+            .withLineColor(Color.BLUE)
+        },
+        annotationConfig = AnnotationConfig(),
+        onClick = {
+          clickedAnnotation = it
+          true
+        }
+      )
+    }
+    composeTestRule.onNodeWithTag(testTag).performClick()
+    composeTestRule.waitUntil { clickedAnnotation != null }
+    assertEquals(Color.BLUE, clickedAnnotation!!.lineColorInt)
+    assertEquals(POLYLINE_POINTS, clickedAnnotation!!.points)
+  }
+
+  /**
+   * @param cameraCenter coordinate of camera's center.
+   * @param annotationPoint coordinate of annotation to be tested.
+   */
+  private fun setMapContent(
+    cameraCenter: Point,
+    annotationPoint: Point,
+    testTag: String,
+    zoom: Double = ZOOM,
+    annotationBlock: @MapboxMapComposable @Composable () -> Unit,
+  ) {
+    composeTestRule.setContent {
+      // screen coordinate of given annotation which can be used for adding Box with testTag
+      var offset: IntOffset? by remember {
+        mutableStateOf(null)
+      }
+      Box(modifier = Modifier.fillMaxSize()) {
+        MapboxMap(
+          modifier = Modifier.testTag(MAP_TEST_TAG),
+          mapViewportState = MapViewportState().apply {
+            setCameraOptions {
+              zoom(zoom)
+              center(cameraCenter)
+            }
+          },
+          style = {
+            MapStyle(style = Style.DARK)
+          },
+        ) {
+          annotationBlock()
+          MapEffect(Unit) { mapView ->
+            mapView.mapboxMap.awaitStyle()
+            val screenCoordinate = mapView.mapboxMap.pixelForCoordinate(annotationPoint)
+            offset = IntOffset(screenCoordinate.x.toInt(), screenCoordinate.y.toInt())
+          }
+        }
+        if (offset != null) {
+          Box(
+            modifier = Modifier
+                .size(5.dp)
+                .offset { offset ?: IntOffset.Zero }
+                .testTag(testTag)
+          )
+        }
+      }
+    }
+
+    composeTestRule.waitUntil(5_000) {
+      composeTestRule.onAllNodesWithTag(testTag)
+        .fetchSemanticsNodes().size == 1
+    }
+  }
+
+  public companion object {
+    public const val MAP_TEST_TAG: String = "map_tag"
+    public const val ZOOM: Double = 10.0
+    private const val INTERVAL = 0.01
+    public val CLUSTER_POINTS: List<Point> by lazy {
+      mutableListOf<Point>().apply {
+        repeat(10) { x ->
+          repeat(10) { y ->
+            add(
+              Point.fromLngLat(
+                HELSINKI.longitude() + INTERVAL * x,
+                HELSINKI.latitude() + INTERVAL * y
+              )
+            )
+          }
+        }
+      }
+    }
+
+    private val POLYGON_POINTS = listOf(
+      listOf(
+        Point.fromLngLat(24.9349, 60.1725),
+        Point.fromLngLat(24.9429, 60.1725),
+        Point.fromLngLat(24.9429, 60.1673),
+        Point.fromLngLat(24.9349, 60.1673),
+        Point.fromLngLat(24.9349, 60.1725),
+      )
+    )
+
+    private val SINGLE_POINT = Point.fromLngLat(
+      HELSINKI.longitude() - INTERVAL,
+      HELSINKI.latitude() - INTERVAL
+    )
+
+    private val POLYLINE_POINTS = listOf(
+      Point.fromLngLat(24.9439358, 60.1718468),
+      HELSINKI,
+      Point.fromLngLat(24.9389362, 60.1697015)
+    )
+
+    private val POLYLINE_GROUP_POINTS = listOf(
+      POLYLINE_POINTS,
+      listOf(
+        Point.fromLngLat(24.9379887, 60.1748898),
+        Point.fromLngLat(24.9364867, 60.1732249),
+        Point.fromLngLat(24.9338259, 60.1730755)
+      )
+    )
+  }
+}
