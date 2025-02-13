300c31af1655e2cf64c46a28bc270f8c411c8a91
Natig Babayev <natig.babayev@mapbox.com>
2025-02-13 08:03:11 +0200

    Update color theme example to use `*UseTheme` (#2949) (#2950)
    * Update color theme example to use `UseTheme`
    
    * Remove fill-layer usage
    
    * Simplify code
    
    * Update mapbox-maps-android/app/src/main/res/values/strings.xml
    
    
    
    * Update mapbox-maps-android/compose-app/src/main/java/com/mapbox/maps/compose/testapp/examples/style/ColorThemeActivity.kt
    
    
    
    ---------
    
    Co-authored-by: Ramon <ramon.sadornilrivera@mapbox.com>
    Co-authored-by: Peng Liu <peng.liu@mapbox.com>

--- app/src/main/java/com/mapbox/maps/testapp/examples/style/ColorThemeActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/style/ColorThemeActivity.kt
@@ -11,9 +11,12 @@ import com.mapbox.maps.MapboxExperimental
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.dsl.cameraOptions
+import com.mapbox.maps.extension.style.atmosphere.generated.atmosphere
+import com.mapbox.maps.extension.style.atmosphere.generated.getAtmosphere
 import com.mapbox.maps.extension.style.color.colorTheme
 import com.mapbox.maps.extension.style.style
 import com.mapbox.maps.testapp.R
+import com.mapbox.maps.testapp.examples.annotation.AnnotationUtils.showShortToast
 import com.mapbox.maps.testapp.utils.BitmapUtils.bitmapFromDrawableRes
 
 /**
@@ -24,6 +27,7 @@ import com.mapbox.maps.testapp.utils.BitmapUtils.bitmapFromDrawableRes
 class ColorThemeActivity : AppCompatActivity() {
 
   private lateinit var mapboxMap: MapboxMap
+  private var atmosphereUseTheme = true
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -38,6 +42,9 @@ class ColorThemeActivity : AppCompatActivity() {
       mapboxMap.loadStyle(
         style(Style.MAPBOX_STREETS) {
           +colorTheme(base64 = BASE64_ENCODED_RED_THEME)
+          +atmosphere {
+            color(COLOR_GREEN)
+          }
         }
       )
     }
@@ -58,8 +65,22 @@ class ColorThemeActivity : AppCompatActivity() {
         mapboxMap.setStyleColorTheme(base64 = BASE64_ENCODED_RED_THEME)
       }
 
+      R.id.menu_action_color_theme_toggle_atmosphere -> {
+        atmosphereUseTheme = !atmosphereUseTheme
+        showShortToast("Color theme will ${"not ".takeUnless { atmosphereUseTheme } ?: ""}affect atmosphere.")
+        mapboxMap.getStyle {
+          it.getAtmosphere().colorUseTheme(
+            if (atmosphereUseTheme) COLOR_USE_THEME_DEFAULT else COLOR_USE_THEME_NONE
+          )
+        }
+      }
+
       R.id.menu_action_color_theme_reset -> {
         mapboxMap.setStyleColorTheme(colorTheme = null)
+        atmosphereUseTheme = true
+        mapboxMap.getStyle {
+          it.getAtmosphere().colorUseTheme(COLOR_USE_THEME_DEFAULT)
+        }
       }
 
       else -> return super.onOptionsItemSelected(item)
@@ -81,12 +102,15 @@ class ColorThemeActivity : AppCompatActivity() {
      */
     private const val BASE64_ENCODED_RED_THEME =
       "iVBORw0KGgoAAAANSUhEUgAABAAAAAAgCAYAAACM/gqmAAAAAXNSR0IArs4c6QAABSFJREFUeF7t3cFO40AQAFHnBv//wSAEEgmJPeUDsid5h9VqtcMiZsfdPdXVzmVZlo+3ZVm+fr3//L7257Lm778x+prL1ff0/b//H+z/4/M4OkuP/n70Nc7f+nnb+yzb//sY6vxt5xXPn+dP/aH+GsXJekb25izxR/ypZ6ucUefv9g4z2jPP3/HPHwAAgABAABgACIACkAAsAL1SD4yKWQAUAHUBdAG8buKNYoYL8PEX4FcHQAAAAAAAAAAAAAAAAAAAAAAA8LAeGF1mABAABAABQACQbZP7+hk5AwACAAAAAAAAAAAAAAAAAAAAAAAA4EE9AICMx4QBAAAAAAAANgvJsxGQV1dA/PxmMEtxU9YoABQACoC5CgDxX/wvsb2sEf/Ff/Ff/N96l5n73+/5YAB4CeBqx2VvMqXgUfD2npkzBCAXEBeQcrkoa5x/FxAXEBcQF5A2Wy3/t32qNYr8I//Mln+MABgBMAJgBMAIgBEAIwBGAIwAGAEwAmAE4K4eAGCNQIw+qQ0AmQ+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAB/6gEABAB5RgACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAN/UAAPKcAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgEFNODICRtDkDO/gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOhvlPUWem+h9xKQ+V4CUt9wO6KZnn/Pv+ff8z/bW5DFP59CUnJbWSP+iX/iX78znqED/urxnwHAAGAAMAAYAAwABgADgAHAAGAAMAAYAAwABgADoNMcHUAdQAQcAUfAe8xEwH0O86t3IPz8OvClu17WqD/UH+oP9cf1Gdia01d/LQsDgAHAAGAAMAAYAAwABgADgAHAAGAAMAAYAAwABkCnSQwABgACj8Aj8D1mItAMAB1wHfDS3S5r5F/5V/6Vf3XAW12h/mIArHY89iZTAAQA2XtmBKAWqOslyf4rgBXACmAFcIur8k/bJ/mnQTr5V/6Vf+fKv0YAjAAYATACYATACIARACMARgCMABgBMAJgBMAIgBEAIwCdZuiA64AjwAgwAtxjpg6cDlztLlLA7/Pr1gueyr56/jx/5ZzUNeof9Y/6R/0zk4HGAGAAMAAYAAwABgADgAHAAGAAMAAYAAwABgADgAHQaQ4DgAGAgCPgCHiPmTqQOpC1u8gAYACMjAf5V/6Vf+XfmTrQ8l97v8Z/5X8GAAOAAcAAYAAwABgADAAGAAOAAcAAYAAwABgADIBO0xgADAAdCB0IHYgeMxkADAAdkGM7IPbf/pfuWlmj/lH/qH/UPzMZGAwABgADgAHAAGAAMAAYAAwABgADgAHAAGAAMAAYAJ3mMAAYAAg4Ao6A95jJAGAA6EDrQJfuclkj/8q/8q/8O1MHWv47Nv8xABgADAAGAAOAAcAAYAAwABgADAAGAAOAAcAAYAB0msYAYADoQOhA6ED0mMkAYADogBzbAbH/9r/YFWWN+kf9o/5R/8xkYDAAGAAMAAYAA4ABwABgADAAGAAMAAYAA4ABwABgAHSawwBgACDgCDgC3mMmA4ABoAOtA126y2WN/Cv/yr/y70wdaPnv2PzHAGAAMAAYAAwABgADgAHAAGAAMAAYAAwABgADgAHQaRoDgAGgA6EDoQPRYyYDgAGgA3JsB8T+2/9iV5Q16h/1j/pH/TOTgcEAYAAwABgADAAGAAOAAcAAYAAwABgADAAGAAPgyQ2AT4NBIB3ew5dkAAAAAElFTkSuQmCC"
+    private const val COLOR_USE_THEME_DEFAULT = "default"
+    private const val COLOR_USE_THEME_NONE = "none"
+    private const val COLOR_GREEN = "#00ff00"
     private const val LATITUDE = 40.72
     private const val LONGITUDE = -73.99
     private val CENTER = Point.fromLngLat(LONGITUDE, LATITUDE)
     private val START_CAMERA_POSITION = cameraOptions {
       center(CENTER)
-      zoom(11.0)
+      zoom(2.0)
       pitch(45.0)
     }
   }
