21344ee06ce1f336c59c7bbcdadef86c512d2f34
Patrick Leonard <pjleonard37@users.noreply.github.com>
2025-06-02 08:48:27 -0400

    Automate testing new Standard style versions (#3782)
    This PR automates the Maps SDK process for Standard and
    Standard-Satellite testing for new versions of these styles. It has two
    parts:
    
    ### Add Standard/Standard-Satellite styles to our SLA metrics
    Specific changes:
    - Update the `mapbox-maps-internal` pin to include new tests for these
    four styles (`standard`, `standard-rc`, `standard-satellite`,
    `standard-satellite-rc`). They were added in this
    [pr](https://github.com/mapbox/mapbox-maps-internal/pull/1430).
    - Update the SLA runners for iOS and Android to run these new tests
    whenever our existing benchmarks are run.
    Once these changes land, I will add charts for these new tests to the
    Mode Engineering Dashboards for iOS and Android.
    Example chart from a trial run:
    ![Screenshot 2025-05-16 at 4 44
    00 PM](https://github.com/user-attachments/assets/8e11cb98-6b36-41ec-89c2-4b24f83e9444)
    
    ### Enable Mobot testing of RC versions of these styles
    Specific changes:
    - Add an example to iOS and update an example for Android to allow
    pasting a style URL at runtime
    Once this is merged I will update the Mobot test script to include test
    specific features in `standard`, `standard-rc`, `standard-satellite`,
    and `standard-satellite-rc`. We can easily add new test cases for new
    Standard features in the RC versions of these styles.
    
    <img
    src="https://github.com/user-attachments/assets/742eaca8-14f5-4deb-825f-396a6fdad5c7"
    width="300">
    <img
    src="https://github.com/user-attachments/assets/65a133af-eae3-464f-93d3-6d4b38bd4d95"
    width="300">
    
    cc @mapbox/sdk-ci
    
    cc @mapbox/maps-ios
    cc @mapbox/gl-native
    cc @mapbox/maps-android
    
    ---------
    
    Co-authored-by: Release SDK bot for Maps SDK team <maps_sdk_ios@mapbox.com>
    Co-authored-by: Changelog autocreator bot <actions@github.com>
    GitOrigin-RevId: 19690b797f4bb2cb4aeccddd3684b0e99e0ea3ec

--- app/src/main/java/com/mapbox/maps/testapp/examples/StyleSwitchActivity.kt
+++ app/src/main/java/com/mapbox/maps/testapp/examples/StyleSwitchActivity.kt
@@ -1,7 +1,13 @@
 package com.mapbox.maps.testapp.examples
 
 import android.os.Bundle
+import android.webkit.URLUtil
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.geojson.Point
+import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
 import com.mapbox.maps.testapp.databinding.ActivityStyleSwitchBinding
@@ -20,9 +26,17 @@ class StyleSwitchActivity : AppCompatActivity() {
     setContentView(binding.root)
 
     mapboxMap = binding.mapView.mapboxMap
+    mapboxMap.setCamera(
+      CameraOptions.Builder()
+        .center(Point.fromLngLat(-0.1213, 51.5015))
+        .zoom(15.0)
+        .bearing(57.0)
+        .pitch(60.0)
+        .build()
+    )
 
     // Instead of this you can add your default style to the map layout with xml attribute `app:mapbox_styleUri="mapbox://styles/streets-v12"`
-    mapboxMap.loadStyle(Style.MAPBOX_STREETS)
+    mapboxMap.loadStyle(Style.STANDARD)
 
     binding.streetsButton.setOnClickListener {
       mapboxMap.loadStyle(Style.MAPBOX_STREETS)
@@ -33,8 +47,8 @@ class StyleSwitchActivity : AppCompatActivity() {
     binding.darkButton.setOnClickListener {
       mapboxMap.loadStyle(Style.DARK)
     }
-    binding.satelliteStreetsButton.setOnClickListener {
-      mapboxMap.loadStyle(Style.SATELLITE_STREETS)
+    binding.customStyleButton.setOnClickListener {
+      showStyleInputDialog()
     }
     binding.satelliteButton.setOnClickListener {
       mapboxMap.loadStyle(Style.SATELLITE)
@@ -48,5 +62,35 @@ class StyleSwitchActivity : AppCompatActivity() {
     binding.standardSatelliteButton.setOnClickListener {
       mapboxMap.loadStyle(Style.STANDARD_SATELLITE)
     }
+  }
+
+  private fun showStyleInputDialog() {
+    val editText = EditText(this)
+    val dialog = AlertDialog.Builder(this)
+      .setTitle("Mapbox Style URL")
+      .setMessage("Paste the “Share URL” for your public Mapbox style")
+      .setView(editText)
+      .setPositiveButton("Save") { _, _ ->
+        val input = editText.text.toString()
+        saveStyleURLInput(input)
+      }
+      .setNegativeButton("Cancel", null)
+      .create()
+    dialog.show()
+  }
+
+  private fun saveStyleURLInput(input: String) {
+    if (input.isValidUri()) {
+      mapboxMap.loadStyle(input)
+    } else {
+      Toast.makeText(this, "Invalid URL. Please check your Mapbox Studio Style URL.", Toast.LENGTH_SHORT).show()
+    }
+  }
+
+  private fun String.isValidUri(): Boolean {
+    val isMapboxStyleUri = startsWith("mapbox://", ignoreCase = true)
+    val isMapboxAssetUri = startsWith("asset://", ignoreCase = true)
+    val isMapboxFileUri = startsWith("file://", ignoreCase = true)
+    return isMapboxStyleUri || isMapboxAssetUri || isMapboxFileUri || URLUtil.isValidUrl(this)
   }
 }
