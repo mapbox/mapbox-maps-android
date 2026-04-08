f84bdb318ab01ff036924e86eb85e06a71d7f296
Natig Babayev <natig.babayev@mapbox.com>
2026-04-08 10:54:06 +0300

    [maps-android] fix maps-android-ci-ok job (#11902)
    GitOrigin-RevId: db31c94c3fec5c79c034a3b4f89e82bdf3acd133

--- compose-app/src/main/java/com/mapbox/maps/compose/testapp/examples/basic/LazyColumnMapActivity.kt
+++ compose-app/src/main/java/com/mapbox/maps/compose/testapp/examples/basic/LazyColumnMapActivity.kt
@@ -6,13 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.mapbox.maps.compose.testapp.ExampleScaffold
 import com.mapbox.maps.compose.testapp.ui.theme.MapboxMapComposeTheme
 import com.mapbox.maps.extension.compose.MapboxMap
-import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
 
 /**
  * Example showcasing [MapboxMap] inside a [LazyColumn].
