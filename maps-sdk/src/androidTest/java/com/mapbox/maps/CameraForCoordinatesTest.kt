67487af26a25d1fdde6def59db007aca1999c552
Peng Liu <peng.liu@mapbox.com>
2026-05-05 00:07:52 +0300

    [maps-sdk] Fix flaky CameraForCoordinatesTest by using async API (#12706)
    GitOrigin-RevId: 9c2930137e8b6d6dd060a179cf4871d09d65c57b

--- maps-sdk/src/androidTest/java/com/mapbox/maps/CameraForCoordinatesTest.kt
+++ maps-sdk/src/androidTest/java/com/mapbox/maps/CameraForCoordinatesTest.kt
@@ -7,7 +7,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.mapbox.bindgen.Value
 import com.mapbox.geojson.Point
 import com.mapbox.maps.dsl.cameraOptions
-import com.mapbox.maps.util.isEmpty
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -28,25 +27,29 @@ class CameraForCoordinatesTest {
 
   @Before
   fun setUp() {
-    val latch = CountDownLatch(1)
+    val layoutLatch = CountDownLatch(1)
+    val styleLatch = CountDownLatch(1)
     rule.scenario.onActivity {
       val mapOptions = MapInitOptions.getDefaultMapOptions(it).toBuilder()
         .pixelRatio(1F)
         .build()
       mapView = MapView(it, MapInitOptions(it, mapOptions = mapOptions))
       mapboxMap = mapView.mapboxMap
+      mapboxMap.subscribeStyleLoaded {
+        mapboxMap.setStyleProjection(Value.valueOf(hashMapOf("name" to Value.valueOf("mercator"))))
+        styleLatch.countDown()
+      }
       mapboxMap.loadStyle("{}")
       // Hardcoded width and height to avoid issues with different screen sizes
       it.frameLayout.addView(mapView, 200, 200)
       mapView.doOnLayout {
-        latch.countDown()
+        layoutLatch.countDown()
       }
-      mapboxMap.setStyleProjection(Value.valueOf(hashMapOf("name" to Value.valueOf("mercator"))))
     }
-    latch.throwExceptionOnTimeoutMs(timeoutMs = 30_000L)
+    layoutLatch.throwExceptionOnTimeoutMs(timeoutMs = 30_000L)
+    styleLatch.throwExceptionOnTimeoutMs(timeoutMs = 30_000L)
   }
 
-  @OptIn(MapboxDelicateApi::class)
   @Test
   fun basicCameraForCoordinates() {
     countDownLatch = CountDownLatch(1)
@@ -59,24 +62,23 @@ class CameraForCoordinatesTest {
           Point.fromLngLat(2.0, 2.0),
           Point.fromLngLat(4.0, 4.0),
         )
-        val cameraForCoordinates = mapboxMap.cameraForCoordinates(
+        mapboxMap.cameraForCoordinates(
           coordinates = points,
           camera = cameraOptions { },
           coordinatesPadding = null,
           maxZoom = null,
           offset = null,
-        )
-        Assert.assertFalse(cameraForCoordinates.isEmpty)
-        Assert.assertEquals(2.001, cameraForCoordinates.center!!.latitude(), 0.001)
-        Assert.assertEquals(2.001, cameraForCoordinates.center!!.longitude(), 0.001)
-        Assert.assertEquals(5.134, cameraForCoordinates.zoom!!, 0.001)
-        countDownLatch.countDown()
+        ) { cameraForCoordinates ->
+          Assert.assertEquals(2.001, cameraForCoordinates.center!!.latitude(), 0.001)
+          Assert.assertEquals(2.001, cameraForCoordinates.center!!.longitude(), 0.001)
+          Assert.assertEquals(5.134, cameraForCoordinates.zoom!!, 0.001)
+          countDownLatch.countDown()
+        }
       }
     }
     countDownLatch.throwExceptionOnTimeoutMs()
   }
 
-  @OptIn(MapboxDelicateApi::class)
   @Test
   fun antimeridianCameraForCoordinates() {
     countDownLatch = CountDownLatch(1)
@@ -87,24 +89,23 @@ class CameraForCoordinatesTest {
           Point.fromLngLat(-180.0, 10.0),
           Point.fromLngLat(180.0, 20.0),
         )
-        val cameraForCoordinates = mapboxMap.cameraForCoordinates(
+        mapboxMap.cameraForCoordinates(
           coordinates = points,
           camera = cameraOptions { },
           coordinatesPadding = null,
           maxZoom = null,
           offset = null,
-        )
-        Assert.assertFalse(cameraForCoordinates.isEmpty)
-        Assert.assertEquals(0.0, cameraForCoordinates.center!!.longitude(), 0.001)
-        Assert.assertEquals(15.058, cameraForCoordinates.center!!.latitude(), 0.001)
-        Assert.assertEquals(-1.356, cameraForCoordinates.zoom!!, 0.001)
-        countDownLatch.countDown()
+        ) { cameraForCoordinates ->
+          Assert.assertEquals(0.0, cameraForCoordinates.center!!.longitude(), 0.001)
+          Assert.assertEquals(15.058, cameraForCoordinates.center!!.latitude(), 0.001)
+          Assert.assertEquals(-1.356, cameraForCoordinates.zoom!!, 0.001)
+          countDownLatch.countDown()
+        }
       }
     }
     countDownLatch.throwExceptionOnTimeoutMs()
   }
 
-  @OptIn(MapboxDelicateApi::class)
   @Test
   fun antimeridian2CameraForCoordinates() {
     countDownLatch = CountDownLatch(1)
@@ -115,18 +116,18 @@ class CameraForCoordinatesTest {
           Point.fromLngLat(-185.0, 19.5),
           Point.fromLngLat(-150.0, 20.0),
         )
-        val cameraForCoordinates = mapboxMap.cameraForCoordinates(
+        mapboxMap.cameraForCoordinates(
           coordinates = points,
           camera = cameraOptions { },
           coordinatesPadding = null,
           maxZoom = null,
           offset = null,
-        )
-        Assert.assertFalse(cameraForCoordinates.isEmpty)
-        Assert.assertEquals(-167.5, cameraForCoordinates.center!!.longitude(), 0.001)
-        Assert.assertEquals(19.750, cameraForCoordinates.center!!.latitude(), 0.001)
-        Assert.assertEquals(2.006, cameraForCoordinates.zoom!!, 0.001)
-        countDownLatch.countDown()
+        ) { cameraForCoordinates ->
+          Assert.assertEquals(-167.5, cameraForCoordinates.center!!.longitude(), 0.001)
+          Assert.assertEquals(19.750, cameraForCoordinates.center!!.latitude(), 0.001)
+          Assert.assertEquals(2.006, cameraForCoordinates.zoom!!, 0.001)
+          countDownLatch.countDown()
+        }
       }
     }
     countDownLatch.throwExceptionOnTimeoutMs()
