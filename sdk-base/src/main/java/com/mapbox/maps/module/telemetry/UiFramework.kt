f67200d25b43b8412c9138df3421eb6d6525087a
Mai Mai <mai.mai@mapbox.com>
2026-08-04 15:23:53 +0300

    [maps-android] Add FLUTTER UiFramework value for feature telemetry (#16984)
    Adds a FLUTTER value to UiFramework so Flutter-created map instances can
    be identified in the feature telemetry Mode report, alongside the
    existing ANDROID_VIEW and JETPACK_COMPOSE values.
    
    GitOrigin-RevId: c5a0efbc14880ce7fa454c66f1f66b988200cf86

--- sdk-base/src/main/java/com/mapbox/maps/module/telemetry/UiFramework.kt
+++ sdk-base/src/main/java/com/mapbox/maps/module/telemetry/UiFramework.kt
@@ -44,5 +44,11 @@ class UiFramework private constructor(
      */
     @JvmField
     val JETPACK_COMPOSE = UiFramework("jetpack-compose")
+
+    /**
+     * Flutter map (`MapWidget`).
+     */
+    @JvmField
+    val FLUTTER = UiFramework("flutter")
   }
 }
